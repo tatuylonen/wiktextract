9f891ab2d20d0c730389a661d4389a836dc2f285
Tatu Ylonen <ylo@clausal.com>
2018-12-13 21:53:45 +0200

    Updated version number.

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.md", "r") as f:
     long_description = f.read()
 
 setup(name="wiktextract",
-      version="0.1.7",
+      version="0.2.0",
       description="Wiktionary dump file parser and multilingual data extractor",
       long_description=long_description,
       long_description_content_type="text/markdown",
