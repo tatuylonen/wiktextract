2cf735b6a539fc3f72285bf4371bd94c0fe5af37
Folke Will <folko@solhost.org>
2021-05-14 21:45:41 +0200

    Fix #49
    Tested and confirmed that this fixed #49

--- wiktextract/page.py
+++ wiktextract/page.py
@@ -33,7 +33,7 @@ languages_by_code = {x["code"]: x for x in ALL_LANGUAGES}
 # Matches head tag
 head_tag_re = re.compile(r"^(head|Han char)$|" +
                          r"^(" + "|".join(languages_by_code.keys()) + r")"
-                         r"-(plural-noun|plural noun|noun|verb|adj|adv|"
+                         r"-(plural-noun|plural noun|noun|verb|adj|num-adj|adv|"
                          r"name|proper-noun|proper noun|prop|pron|phrase|"
                          r"decl noun|decl-noun|prefix|clitic|number|ordinal|"
                          r"syllable|suffix|affix|pos|gerund|combining form|"
