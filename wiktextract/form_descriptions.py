d3d82849ec4de7a254f73e483c5f583a88235ec4
Tatu Ylonen <ylo@clausal.com>
2022-01-19 22:12:55 +0200

    Changed parenthesized part handling in word head processing. Now should only remove those parenthesized parts from the head word that start or end with a space or are at the beginning or end.

--- wiktextract/form_descriptions.py
+++ wiktextract/form_descriptions.py
@@ -1199,7 +1199,11 @@ def parse_word_head(ctx, pos, text, data, is_reconstruction):
         return
 
     # Handle the part of the head that is not in parentheses
-    base = re.sub(r"(\s?)\^?\(([^()]|\([^(]*\))*\)", r"\1", text)
+    base = text
+    base = re.sub(r"\(([^()]|\([^(]*\))*\)($|\s)", r"\2", base)
+    base = re.sub(r"(^|\s)\(([^()]|\([^(]*\))*\)", r"\1", base)
+    base = re.sub(r"(\s?)\^\(([^()]|\([^(]*\))*\)", r"\1", base)
+    print("CLEANED BASE:", base)
     base = re.sub(r"\?", " ", base)  # Removes uncertain articles etc
     base = re.sub(r"\s+", " ", base)
     base = re.sub(r" ([,;])", r"\1", base)
