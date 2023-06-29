55d1c090a9facd425ef5e89d0863fe94793c7b4b
xxyzz <gitpull@protonmail.com>
2023-06-29 17:26:03 +0800

    Extract linkage data from table expanded from the `zh-dial` template

--- wiktextract/extractor/zh/linkage.py
+++ wiktextract/extractor/zh/linkage.py
@@ -51,6 +51,10 @@ def extract_linkages(
                     extract_saurus_template(
                         wxr, node, page_data, linkage_type, sense
                     )
+                elif template_name == "zh-dial":
+                    extract_zh_dial_template(
+                        wxr, node, page_data, linkage_type, sense
+                    )
                 else:
                     expanded_node = wxr.wtp.parse(
                         wxr.wtp.node_to_wikitext(node), expand_all=True
@@ -75,34 +79,93 @@ def extract_saurus_template(
     node: WikiNode,
     page_data: Dict,
     linkage_type: str,
-    sense: str
+    sense: Optional[str],
 ) -> None:
     from wiktextract.thesaurus import search_thesaurus
 
     thesaurus_page_title = node.args[-1][0]
     for thesaurus in search_thesaurus(
-            wxr.thesaurus_db_conn,
-            thesaurus_page_title,
-            page_data[-1].get("lang_code"),
-            page_data[-1].get("pos"),
-            linkage_type,
+        wxr.thesaurus_db_conn,
+        thesaurus_page_title,
+        page_data[-1].get("lang_code"),
+        page_data[-1].get("pos"),
+        linkage_type,
     ):
+        if thesaurus.term == wxr.wtp.title:
+            continue
         linkage_data = {"word": thesaurus.term}
         if thesaurus.roman is not None:
             linkage_data["roman"] = thesaurus.roman
         if thesaurus.tags is not None:
             linkage_data["tags"] = thesaurus.tags.split("|")
         if thesaurus.language_variant is not None:
-            linkage_data[
-                "language_variant"
-            ] = thesaurus.language_variant
+            linkage_data["language_variant"] = thesaurus.language_variant
         if sense is not None:
             linkage_data["sense"] = sense
         elif thesaurus.sense is not None:
             linkage_data["sense"] = thesaurus.sense
-        add_linkage_data(
-            wxr, page_data, linkage_type, linkage_data
-        )
+        add_linkage_data(wxr, page_data, linkage_type, linkage_data)
+
+
+def extract_zh_dial_template(
+    wxr: WiktextractContext,
+    node: Union[WikiNode, str],
+    page_data: Dict,
+    linkage_type: str,
+    sense: Optional[str],
+) -> None:
+    dial_data = {}
+    node = wxr.wtp.parse(
+        wxr.wtp.node_to_wikitext(node), expand_all=True
+    )
+    extract_zh_dial_recursively(wxr, node, dial_data, None)
+    for term, tags in dial_data.items():
+        linkage_data = {"word": term}
+        if sense is not None:
+            linkage_data["sense"] = sense
+        if len(tags) > 0:
+            linkage_data["tags"] = tags
+        add_linkage_data(wxr, page_data, linkage_type, linkage_data)
+
+
+def extract_zh_dial_recursively(
+    wxr: WiktextractContext,
+    node: Union[WikiNode, str],
+    dial_data: Dict[str, List[str]],
+    header_lang: Optional[str],
+) -> str:
+    if isinstance(node, WikiNode) and node.kind == NodeKind.TABLE_ROW:
+        tags = []
+        for child in node.children:
+            if isinstance(child, WikiNode):
+                if child.kind == NodeKind.TABLE_HEADER_CELL:
+                    header_lang = clean_node(wxr, None, child)
+                elif child.kind == NodeKind.TABLE_CELL:
+                    tags.append(clean_node(wxr, None, child))
+        if len(tags) < 1:  # table header
+            return
+        terms = tags[-1].removesuffix(" dated")
+        tags = tags[:-1]
+        if header_lang is not None:
+            tags = [header_lang] + tags
+        for term in terms.split("、"):
+            if term == wxr.wtp.title:
+                continue
+            old_tags = dial_data.get(term, [])
+            old_tag_set = set(old_tags)
+            new_tags = old_tags
+            for tag in tags:
+                if tag not in old_tag_set:
+                    new_tags.append(tag)
+            dial_data[term] = new_tags
+    elif isinstance(node, WikiNode):
+        for child in node.children:
+            returned_lang = extract_zh_dial_recursively(
+                wxr, child, dial_data, header_lang
+            )
+            if returned_lang is not None:
+                header_lang = returned_lang
+    return header_lang
 
 
 def add_linkage_data(
