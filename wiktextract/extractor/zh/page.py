57cd8505f58f5292386bccc295b15c33f4eb28a6
xxyzz <gitpull@protonmail.com>
2023-07-04 15:56:18 +0800

    Handle the mannualy created example list case
    The second example sentence in page https://zh.wiktionary.org/wiki/%
    is list not created from template. Wikitext:
    
    ```
    \#* [url]{{template}} (pdf) from [url]
    \#*: example text).
    ```

--- wiktextract/extractor/zh/page.py
+++ wiktextract/extractor/zh/page.py
@@ -10,6 +10,7 @@ from wiktextract.datautils import data_append
 from wiktextract.page import clean_node, LEVEL_KINDS
 from wiktextract.wxr_context import WiktextractContext
 
+from ..share import contains_list
 from .linkage import extract_linkages
 from .pronunciation import extract_pronunciation_recursively
 
@@ -258,65 +259,89 @@ def extract_examples(
     elif isinstance(node, WikiNode):
         if node.kind == NodeKind.LIST_ITEM:
             example_data = {"type": "example"}
-            for child in node.children:
-                if (
-                    isinstance(child, WikiNode)
-                    and child.kind == NodeKind.TEMPLATE
-                ):
-                    template_name = child.args[0][0].strip()
-                    expanded_text = clean_node(wxr, None, child)
-                    if template_name == "quote-book":
+            # example text in the nested list
+            # https://zh.wiktionary.org/wiki/%, the second example
+            if contains_list(node.children):
+                for index, child_node in enumerate(node.children):
+                    if (
+                        isinstance(child_node, WikiNode)
+                        and child_node.kind == NodeKind.LIST
+                    ):
                         example_data["type"] = "quote"
-                        for line_num, expanded_line in enumerate(
-                            expanded_text.splitlines()
+                        example_data["ref"] = clean_node(
+                            wxr, None, node.children[:index]
+                        )
+                        example_data["text"] = clean_node(
+                            wxr, None, child_node.children[0].children
+                        )
+            else:
+                # parse example templates
+                for child in node.children:
+                    if (
+                        isinstance(child, WikiNode)
+                        and child.kind == NodeKind.TEMPLATE
+                    ):
+                        template_name = child.args[0][0].strip()
+                        expanded_text = clean_node(wxr, None, child)
+                        if (
+                            template_name == "quote-book"
+                            or template_name.startswith("RQ:")
                         ):
-                            if line_num == 0:
-                                key = "ref"
-                            elif line_num == 1:
-                                key = "text"
-                            elif line_num == 2 and any(
-                                template_arg[0].startswith("transliteration=")
-                                for template_arg in child.args
-                                if len(template_arg) > 0
-                                and isinstance(template_arg[0], str)
+                            example_data["type"] = "quote"
+                            for line_num, expanded_line in enumerate(
+                                expanded_text.splitlines()
                             ):
-                                key = "roman"
-                            else:
-                                key = "translation"
-                            if expanded_line != "（請為本引文添加中文翻譯）":
-                                example_data[key] = expanded_line
-                    elif template_name == "ja-usex":
-                        for line_num, expanded_line in enumerate(
-                            expanded_text.splitlines()
-                        ):
-                            if line_num == 0:
-                                key = "text"
-                            elif line_num == 1:
-                                key = "roman"
-                            else:
-                                key = "translation"
-                            example_data[key] = expanded_line
-                    elif template_name in {"zh-x", "zh-usex"}:
-                        for expanded_line in expanded_text.splitlines():
-                            if expanded_line.endswith("體]"):
-                                # expanded simplified or traditional Chinese
-                                # example sentence usually ends with
-                                # "繁體]" or "簡體]"
-                                if example_data.get("texts") is not None:
-                                    example_data["texts"].append(expanded_line)
+                                if line_num == 0:
+                                    key = "ref"
+                                elif line_num == 1:
+                                    key = "text"
+                                elif line_num == 2 and any(
+                                    template_arg[0].startswith(
+                                        "transliteration="
+                                    )
+                                    for template_arg in child.args
+                                    if len(template_arg) > 0
+                                    and isinstance(template_arg[0], str)
+                                ):
+                                    key = "roman"
                                 else:
-                                    example_data["texts"] = [expanded_line]
-                            elif expanded_line.endswith("]"):
-                                example_data["roman"] = expanded_line
-                            elif expanded_line.startswith("來自："):
-                                example_data["ref"] = expanded_line[3:]
-                                example_data["type"] = "quote"
-                            else:
-                                example_data["translation"] = expanded_line
-                    else:
-                        example_data["text"] = expanded_text
+                                    key = "translation"
+                                if expanded_line != "（請為本引文添加中文翻譯）":
+                                    example_data[key] = expanded_line
+                        elif template_name == "ja-usex":
+                            for line_num, expanded_line in enumerate(
+                                expanded_text.splitlines()
+                            ):
+                                if line_num == 0:
+                                    key = "text"
+                                elif line_num == 1:
+                                    key = "roman"
+                                else:
+                                    key = "translation"
+                                example_data[key] = expanded_line
+                        elif template_name in {"zh-x", "zh-usex"}:
+                            for expanded_line in expanded_text.splitlines():
+                                if expanded_line.endswith("體]"):
+                                    # expanded simplified or traditional Chinese
+                                    # example sentence usually ends with
+                                    # "繁體]" or "簡體]"
+                                    if example_data.get("texts") is not None:
+                                        example_data["texts"].append(
+                                            expanded_line
+                                        )
+                                    else:
+                                        example_data["texts"] = [expanded_line]
+                                elif expanded_line.endswith("]"):
+                                    example_data["roman"] = expanded_line
+                                elif expanded_line.startswith("來自："):
+                                    example_data["ref"] = expanded_line[3:]
+                                    example_data["type"] = "quote"
+                                else:
+                                    example_data["translation"] = expanded_line
+                        else:
+                            example_data["text"] = expanded_text
 
-            if "text" in example_data:
+            if "text" in example_data or "texts" in example_data:
                 data_append(
                     wxr, page_data[-1]["senses"][-1], "examples", example_data
                 )
