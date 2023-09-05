58746fb40e564cb13b336dc5f41fc60542ea7855
xxyzz <gitpull@protonmail.com>
2023-09-05 14:40:25 +0800

    Extract equivalent forms data from French Wiktionary "équiv-pour" template
    equivalent male form, female form, etc.

--- tests/test_fr_form_line.py
+++ tests/test_fr_form_line.py
@@ -23,15 +23,41 @@ class TestFormLine(unittest.TestCase):
         )
 
     @patch(
-        "wiktextract.extractor.fr.form_line.clean_node",
-        return_value="/lə nɔ̃/",
+        "wiktextract.extractor.fr.form_line.clean_node", return_value="/lə nɔ̃/"
     )
-    def test_pron_and_gender(self, mock_clean_node):
+    def test_ipa(self, mock_clean_node):
         self.wxr.wtp.start_page("")
-        root = self.wxr.wtp.parse("'''le nom''' {{pron|lə nɔ̃|fr}} {{m}}")
+        root = self.wxr.wtp.parse("'''le nom''' {{pron|lə nɔ̃|fr}}")
+        page_data = [defaultdict(list)]
+        extract_form_line(self.wxr, page_data, root.children)
+        self.assertEqual(page_data, [{"sounds": [{"ipa": "/lə nɔ̃/"}]}])
+
+    @patch(
+        "wiktextract.extractor.fr.form_line.clean_node", return_value="masculin"
+    )
+    def test_gender(self, mock_clean_node):
+        self.wxr.wtp.start_page("")
+        root = self.wxr.wtp.parse("'''le nom''' {{m}}")
+        page_data = [defaultdict(list)]
+        extract_form_line(self.wxr, page_data, root.children)
+        self.assertEqual(page_data, [{"tags": ["masculin"]}])
+
+    def test_equiv_pour(self):
+        self.wxr.wtp.start_page("")
+        root = self.wxr.wtp.parse(
+            "{{équiv-pour|une femme|autrice|auteure|auteuse|lang=fr}}"
+        )
         page_data = [defaultdict(list)]
         extract_form_line(self.wxr, page_data, root.children)
         self.assertEqual(
             page_data,
-            [{"sounds": [{"ipa": "/lə nɔ̃/"}], "tags": ["masculine"]}],
+            [
+                {
+                    "forms": [
+                        {"form": "autrice", "tags": ["pour une femme"]},
+                        {"form": "auteure", "tags": ["pour une femme"]},
+                        {"form": "auteuse", "tags": ["pour une femme"]},
+                    ]
+                }
+            ],
         )
--- wiktextract/extractor/fr/form_line.py
+++ wiktextract/extractor/fr/form_line.py
@@ -6,15 +6,7 @@ from wiktextract.page import clean_node
 from wiktextract.wxr_context import WiktextractContext
 
 
-GENDER_TEMPLATES = {
-    "m": "masculine",
-    "f": "feminine",
-    "n": "neuter",
-    "c": "common",
-    "mf": "masculine and feminine identical",
-    "mf ?": "masculine or feminine (usage hesitates)",
-    "fm ?": "feminine or masculine (usage hesitates)",
-}
+GENDER_TEMPLATES = {"m", "f", "n", "c", "mf", "mf ?", "fm ?"}
 
 
 def extract_form_line(
@@ -36,6 +28,18 @@ def extract_form_line(
                     {"ipa": clean_node(wxr, None, node)}
                 )
             elif node.template_name in GENDER_TEMPLATES:
-                page_data[-1]["tags"].append(
-                    GENDER_TEMPLATES.get(node.template_name)
-                )
+                gender_text = clean_node(wxr, None, node)  # category
+                page_data[-1]["tags"].append(gender_text)
+            elif node.template_name == "équiv-pour":
+                process_equiv_pour_template(node, page_data)
+
+
+def process_equiv_pour_template(node: WikiNode, page_data: List[Dict]) -> None:
+    # equivalent form: https://fr.wiktionary.org/wiki/Modèle:équiv-pour
+    form_type = node.template_parameters.get(1)
+    for template_arg_index in range(2, 8):
+        form = node.template_parameters.get(template_arg_index)
+        if form is not None:
+            page_data[-1]["forms"].append(
+                {"form": form, "tags": [f"pour {form_type}"]}
+            )
