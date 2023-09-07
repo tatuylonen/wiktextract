6d17140bb0a572dbd359b980c3436be914295d2c
xxyzz <gitpull@protonmail.com>
2023-09-07 16:10:14 +0800

    Move `create_audio_url_dict()` to "extractor/share.py" file
    This function could be used in French Wiktionary extractor code.

--- wiktextract/extractor/share.py
+++ wiktextract/extractor/share.py
@@ -1,10 +1,9 @@
+import hashlib
 import re
 from html import unescape
-from typing import Iterable, List, Optional, Tuple, Union
+from typing import Dict, Iterable, List, Optional, Tuple, Union
 
 from wikitextprocessor import WikiNode
-
-WIKIMEDIA_COMMONS_URL = "https://commons.wikimedia.org/wiki/Special:FilePath/"
 
 
 def strip_nodes(
@@ -49,3 +48,35 @@ def split_chinese_variants(text: str) -> Iterable[Tuple[Optional[str], str]]:
                 yield "zh-Hant" if variant_index == 0 else "zh-Hans", variant
     else:
         yield None, text
+
+
+def create_audio_url_dict(filename: str) -> Dict[str, str]:
+    file_url_key = filename[filename.rfind(".") + 1 :].lower() + "_url"
+    filename_without_prefix = filename.removeprefix("File:")
+    audio_dict = {
+        "audio": filename_without_prefix,
+        file_url_key: "https://commons.wikimedia.org/wiki/Special:FilePath/"
+        + filename_without_prefix,
+    }
+    transcode_formates = []
+    if not filename.lower().endswith((".oga", ".ogg")):
+        transcode_formates.append("ogg")
+    if not filename.lower().endswith(".mp3"):
+        transcode_formates.append("mp3")
+    for file_suffix in transcode_formates:
+        audio_dict[f"{file_suffix}_url"] = create_transcode_url(
+            filename_without_prefix.replace(" ", "_"), file_suffix
+        )
+    return audio_dict
+
+
+def create_transcode_url(filename: str, transcode_suffix: str) -> str:
+    # Chinese Wiktionary template might expands filename that has the a lower
+    # first letter but the actual Wikimedia Commons file's first letter is
+    # capitalized
+    filename = filename[0].upper() + filename[1:]
+    md5 = hashlib.md5(filename.encode()).hexdigest()
+    return (
+        "https://upload.wikimedia.org/wikipedia/commons/transcoded/"
+        + f"{md5[0]}/{md5[:2]}/{filename}/{filename}.{transcode_suffix}"
+    )
