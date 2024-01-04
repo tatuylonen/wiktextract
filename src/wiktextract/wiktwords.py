tatuylonen/wiktextract

commit 7c835658145ab7c8c13f75d6a25d41d92c3c7065

--statistics never implemented, disabled
--statistics command line parameter was never implemented.

There are bits of logic and code left over from the previous
attempt, like in merge_return that assumes somewhere certain
statistics data has been collected to be handled in the
central process, but that statistics collection has never
actually been done except for one place: wxr.config.section_counts
gets incremented in en/page.py, BUT even that data is then
discarded if multiprocessing is used and merging is required
(because there is no data sent with the merge).

Using --statistics doesn't actually print any statistics, except
if you are processing one page (or one process): then you get
section_counts.

What is currently typed as CollagedErrorReturnData is assumed to be
actually a larger collection that includes these count statistics.

I've commented out the code related to all of this; nothing
should break, because nothing actually touches this.

To make it easier to find all these random bits, I've commented
them with the term `STATISTICS_IMPLEMENTATION`.

src/wiktextract/wiktwords.py
@@ -212,12 +212,14 @@ def main():
         default=False,
         help="Capture descendants",
     )
-    parser.add_argument(
-        "--statistics",
-        action="store_true",
-        default=False,
-        help="Print statistics",
-    )
+    # XXX Was never implemented fully.
+    # Search: STATISTICS_IMPLEMENTATION
+    # parser.add_argument(
+    #     "--statistics",
+    #     action="store_true",
+    #     default=False,
+    #     help="Print statistics",
+    # )
     parser.add_argument(
         "--page",
         type=str,
@@ -521,6 +523,7 @@ def main():
             pass
         os.rename(out_tmp_path, out_path)
 
+    # XXX was never implemented fully. Search for: STATISTICS_IMPLEMENTATION
     if args.statistics:
         print("")
         print("LANGUAGE COUNTS")

src/wiktextract/config.py
@@ -96,9 +96,9 @@ class WiktionaryConfig:
         self.expand_tables = expand_tables
         # Some fields for statistics
         self.num_pages = 0
-        self.language_counts = collections.defaultdict(int)
-        self.pos_counts = collections.defaultdict(int)
-        self.section_counts = collections.defaultdict(int)
+        self.language_counts: dict[str, int] = collections.defaultdict(int)
+        self.pos_counts: dict[str, int] = collections.defaultdict(int)
+        self.section_counts: dict[str, int] = collections.defaultdict(int)
         # Some fields related to errors
         # The word currently being processed.
         self.word = None
@@ -114,14 +114,18 @@ class WiktionaryConfig:
         self.load_edition_settings()
 
     def merge_return(self, ret: CollatedErrorReturnData):
-        if "num_pages" in ret:
-            self.num_pages += ret["num_pages"]
-            for k, v in ret["language_counts"].items():
-                self.language_counts[k] += v
-            for k, v in ret["pos_counts"].items():
-                self.pos_counts[k] += v
-            for k, v in ret["section_counts"].items():
-                self.section_counts[k] += v
+        # XXX This was never properly implemented; even the only
+        # count (self.section_counts) that is updated during running
+        # gets discarded when doing batches instead of individual
+        # pages. Search: STATISTICS_IMPLEMENTATION
+        # if "num_pages" in ret:
+        #     self.num_pages += ret["num_pages"]
+        #     for k, v in ret["language_counts"].items():
+        #         self.language_counts[k] += v
+        #     for k, v in ret["pos_counts"].items():
+        #         self.pos_counts[k] += v
+        #     for k, v in ret["section_counts"].items():
+        #         self.section_counts[k] += v
         if "errors" in ret:
             self.errors.extend(ret.get("errors", []))
             self.warnings.extend(ret.get("warnings", []))

src/wiktextract/extractor/en/page.py
@@ -2914,6 +2914,8 @@ def parse_language(wxr, langnode, language, lang_code):
             t = clean_node(wxr, etym_data,
                            node.sarg if node.sarg else node.largs)
             t = t.lower()
+            # XXX these counts were never implemented fully, and even this
+            # gets discarded: Search STATISTICS_IMPLEMENTATION
             wxr.config.section_counts[t] += 1
             # print("PROCESS_CHILDREN: T:", repr(t))
             if t.startswith(tuple(wxr.config.OTHER_SUBTITLES["pronunciation"])):

commit 89894ccda064d3cca88dab651266387511eb294b

Fix `no attribute 'statistics'` bug introduced in 7c83565
Delete these code instead of comment them out, use git to track the
code.

src/wiktextract/wiktwords.py
@@ -200,14 +200,6 @@ def main():
         default=False,
         help="Capture descendants",
     )
-    # XXX Was never implemented fully.
-    # Search: STATISTICS_IMPLEMENTATION
-    # parser.add_argument(
-    #     "--statistics",
-    #     action="store_true",
-    #     default=False,
-    #     help="Print statistics",
-    # )
     parser.add_argument(
         "--page",
         type=str,
@@ -510,33 +502,6 @@ def main():
         except FileNotFoundError:
             pass
         os.rename(out_tmp_path, out_path)
-
-    # XXX was never implemented fully. Search for: STATISTICS_IMPLEMENTATION
-    if args.statistics:
-        print("")
-        print("LANGUAGE COUNTS")
-        for k, cnt in sorted(
-            wxr.config.language_counts.items(), key=lambda x: -x[1]
-        ):
-            print("  {:>7d} {}".format(cnt, k))
-            if cnt < 1000:
-                break
-        print("  ...")
-        print("")
-
-        print("")
-        print("POS HEADER USAGE")
-        for k, cnt in sorted(
-            wxr.config.pos_counts.items(), key=lambda x: -x[1]
-        ):
-            print("  {:>7d} {}".format(cnt, k))
-
-        print("")
-        print("POS SUBSECTION HEADER USAGE")
-        for k, cnt in sorted(
-            wxr.config.section_counts.items(), key=lambda x: -x[1]
-        ):
-            print("  {:>7d} {}".format(cnt, k))
 
     if args.errors:
         with open(args.errors, "w", encoding="utf-8") as f: