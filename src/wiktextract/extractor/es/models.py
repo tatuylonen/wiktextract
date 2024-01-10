00746bbe3c366857a2dcde0c293fc5bf85f33932
Kristian Järventaus <kristian@clausal.com>
2024-01-10 11:45:08 +0200

    Spanish edition: Change pydantic classes
    Fields with singular names should only contain singular
    items and not lists; in this case, I've opted to change
    the type (list -> Optional[str]) instead of changing the
    names ("ipa" -> "ipas").
    
    We never use "tag" as a field, although it's not incorrect;
    in this case, I changed the name to "tags" and left it as a
    list of strings. The strings should be space separated
    tags, this field should not be used for arbitrary ad-hoc 'tags'!

--- src/wiktextract/extractor/es/models.py
+++ src/wiktextract/extractor/es/models.py
@@ -121,24 +121,24 @@ class Spelling(BaseModelWrap):
 
 
 class Sound(BaseModelWrap):
-    ipa: list[str] = Field(
-        default=[], description="International Phonetic Alphabet"
+    ipa: Optional[str] = Field(
+        default=None, description="International Phonetic Alphabet"
     )
-    phonetic_transcription: list[str] = Field(
-        default=[], description="Phonetic transcription, less exact than IPA."
+    phonetic_transcription: Optional[str] = Field(
+        default=None, description="Phonetic transcription, less exact than IPA."
     )
-    audio: list[str] = Field(default=[], description="Audio file name")
-    wav_url: list[str] = Field(default=[])
-    ogg_url: list[str] = Field(default=[])
-    mp3_url: list[str] = Field(default=[])
-    flac_url: list[str] = Field(default=[])
-    roman: list[str] = Field(
-        default=[], description="Translitaration to Roman characters"
+    audio: Optional[str] = Field(default=None, description="Audio file name")
+    wav_url: Optional[str] = Field(default=None)
+    ogg_url: Optional[str] = Field(default=None)
+    mp3_url: Optional[str] = Field(default=None)
+    flac_url: Optional[str] = Field(default=None)
+    roman: Optional[str] = Field(
+        default=None, description="Translitaration to Roman characters"
     )
-    syllabic: list[str] = Field(
-        default=[], description="Syllabic transcription"
+    syllabic: Optional[str] = Field(
+        default=None, description="Syllabic transcription"
     )
-    tag: list[str] = Field(
+    tags: list[str] = Field(
         default=[], description="Specifying the variant of the pronunciation"
     )
 
