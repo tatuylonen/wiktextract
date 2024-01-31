372b1172f392b82bdc9302dc77bd45b695769fbd
Kristian Järventaus <kristian@clausal.com>
2024-01-31 09:46:08 +0200

    Es-edition: Remove kludges, use substring "whitelist"
    Really, all the previous kludges in the previous commit are
    unnecessary if I'd just realized we only want to keep
    etymology template data for *etymology templates*, not
    just any template. So a simple substring check should suffice.
    It is unlikely that there are any conflicting templates
    with "etim" in their name, but possible.

--- src/wiktextract/extractor/es/etymology.py
+++ src/wiktextract/extractor/es/etymology.py
@@ -1,17 +1,9 @@
+from typing import cast
 from wikitextprocessor import NodeKind, WikiNode
+from wikitextprocessor.parser import TemplateNode
 from wiktextract.extractor.es.models import EtymologyTemplate, WordEntry
-from wiktextract.page import clean_node, LEVEL_KINDS
+from wiktextract.page import clean_node
 from wiktextract.wxr_context import WiktextractContext
-
-
-IGNORED_NODE_LEVELS = [NodeKind.HTML] + list(LEVEL_KINDS)
-
-SKIPPED_TEMPLATES = (
-    "ampliable",
-    "arcoiris",
-    "clear",
-    "cita requerida",
-)
 
 
 def process_etymology_block(
@@ -33,25 +25,14 @@ def process_etymology_block(
     """
 
     has_etymology_info = False
-    ignore_these_templates: list[WikiNode] = []
-    for ignored_node in level_node.find_child_recursively(IGNORED_NODE_LEVELS):
-        # stuff inside <ref></ref> should be ignored, and obvious mistakes
-        # like a sub-section to etymology (wrong level kind, in es.wiktionary
-        # these should all apparently be sibligns, so Etymology sections
-        # shouldn't have other levels as their children (example:
-        # calzado around 2024-01-30
-        if ignored_node.kind == NodeKind.HTML and ignored_node.sarg == "reg":
-            # HTML nodes other than REG should be fine
-            continue
-        ignore_these_templates.extend(
-            ignored_node.find_child_recursively(NodeKind.TEMPLATE)
-        )
 
     for template_node in level_node.find_child_recursively(NodeKind.TEMPLATE):
-        if template_node in ignore_these_templates:
-            continue
-
-        if template_node.template_name in SKIPPED_TEMPLATES:
+        # no-op type-annotation cast; we softly assert template_node is a
+        # TemplateNode, which has .template_name, to quiet the type-checker.
+        template_node = cast(TemplateNode, template_node)
+        if "etim" not in template_node.template_name:
+            # We don't want to keep any other template data other than
+            # the main etymology templates (and maybe Plantilla:etim)
             continue
 
         entry.etymology_templates = entry.etymology_templates or []
@@ -63,7 +44,7 @@ def process_etymology_block(
 
         if etymology_template.expansion in (
             # "Please fill in this etymology, thank you..."
-            "Si puedes, incorpórala: ver cómo",
+            "Si puedes, incorpórala: ver cómo.",
             "Préstamo no adaptado.",
             "Este lema en este idioma es ampliable. "
             "Retira este aviso si la mayor parte de las acepciones ya están incluidas.",
@@ -77,13 +58,11 @@ def process_etymology_block(
                 if isinstance(param, str)
                 else clean_node(wxr, None, param)
             )
+            # if any other index other than "leng" is encountered,
+            # has_etymology => True
             has_etymology_info = has_etymology_info or index != "leng"
-        if args:
+        if args and not (len(args) == 1 and "leng" in args):
             etymology_template.args = args
-
-        # DEBUG
-        if not args:
-            print(f"EMPTY ARGS in {entry.word}, {etymology_template}")
 
         entry.etymology_templates.append(etymology_template)
 
