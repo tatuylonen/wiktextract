902774a868d1c3977eaf535d4d282543326172a6
xxyzz <gitpull@protonmail.com>
2024-02-07 15:44:16 +0800

    Remove "Voir la conjugaison du verbe *" form tags in fr edition
    It's a conjugaison page link at the top of forms table.

--- src/wiktextract/extractor/fr/inflection.py
+++ src/wiktextract/extractor/fr/inflection.py
@@ -31,6 +31,9 @@ IGNORE_TABLE_HEADERS = frozenset(
         "commun",  # sv-nom-c-ar
     }
 )
+IGNORE_TABLE_HEADER_PREFIXES = (
+    "voir la conjugaison du verbe",  # Modèle:fr-verbe-flexion
+)
 IGNORE_TABLE_CELL = frozenset(
     {
         "Déclinaisons",  # de-adj
@@ -110,6 +113,9 @@ def process_inflection_table(
                     ).replace("\n", " ")
                     if (
                         table_header_text.lower() in IGNORE_TABLE_HEADERS
+                        or table_header_text.lower().startswith(
+                            IGNORE_TABLE_HEADER_PREFIXES
+                        )
                         or len(table_header_text.strip()) == 0
                     ):
                         continue
