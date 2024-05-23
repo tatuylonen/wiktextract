15c6a2594b411787664bdaef3dc5e5f3f35fbcac
xxyzz <gitpull@protonmail.com>
2024-05-23 18:32:30 +0800

    [de] extract verb inflection tables in "Flexion" namespace
    I think `clean_node()` doesn't return the correct string for
    `[[Flexion:sehend]]`, it returns `sehand` but should be
    `Flexion:sehend`.

--- src/wiktextract/extractor/de/flexion.py
+++ src/wiktextract/extractor/de/flexion.py
@@ -9,7 +9,7 @@ from .models import Form, WordEntry
 from .tags import translate_raw_tags
 
 
-def parse_adj_flexion_page(
+def parse_flexion_page(
     wxr: WiktextractContext, word_entry: WordEntry, page_title: str
 ) -> None:
     # https://de.wiktionary.org/wiki/Hilfe:Flexionsseiten
@@ -26,10 +26,14 @@ def parse_adj_flexion_page(
             process_deklinationsseite_template(
                 wxr, word_entry, flexion_template, page_title
             )
+        elif flexion_template.template_name.startswith("Deutsch Verb"):
+            process_deutsch_verb_template(
+                wxr, word_entry, flexion_template, page_title
+            )
 
 
 @dataclass
-class ColspanHeader:
+class SpanHeader:
     text: str
     index: int
     span: int
@@ -72,7 +76,7 @@ def process_deklinationsseite_template(
                                 has_article = False
                                 col_headers.clear()
                             col_headers.append(
-                                ColspanHeader(cell_text, col_index, col_span)
+                                SpanHeader(cell_text, col_index, col_span)
                             )
                             col_index += col_span
                         else:
@@ -102,3 +106,103 @@ def process_deklinationsseite_template(
                                 translate_raw_tags(form)
                                 word_entry.forms.append(form)
                         col_index += int(cell_node.attrs.get("colspan", "1"))
+
+
+def process_deutsch_verb_template(
+    wxr: WiktextractContext,
+    word_entry: WordEntry,
+    template_node: TemplateNode,
+    page_tite: str,
+) -> None:
+    # Vorlage:Deutsch Verb regelmäßig
+    expanded_template = wxr.wtp.parse(
+        wxr.wtp.node_to_wikitext(template_node), expand_all=True
+    )
+    for table in expanded_template.find_child_recursively(NodeKind.TABLE):
+        col_headers = []
+        for row in table.find_child(NodeKind.TABLE_ROW):
+            row_header = ""
+            col_index = 0
+            col_header_index = 0
+            is_bold_col_header = all(
+                c.contain_node(NodeKind.BOLD)
+                for c in row.find_child(NodeKind.TABLE_CELL)
+                if clean_node(wxr, None, c) != ""
+            )
+            if (
+                len(
+                    list(
+                        row.find_child(
+                            NodeKind.TABLE_HEADER_CELL | NodeKind.TABLE_CELL
+                        )
+                    )
+                )
+                == 1
+            ):
+                col_headers.clear()  # new table
+            for cell in row.find_child(
+                NodeKind.TABLE_HEADER_CELL | NodeKind.TABLE_CELL
+            ):
+                cell_text = clean_node(wxr, None, cell)
+                if cell_text in (
+                    "Flexion der Verbaladjektive",
+                    "(nichterweiterte) Infinitive",
+                ):
+                    break
+                elif (
+                    cell.kind == NodeKind.TABLE_HEADER_CELL
+                    and cell_text not in ("", "Person")
+                ):
+                    col_headers.append(
+                        SpanHeader(
+                            cell_text,
+                            col_header_index,
+                            int(cell.attrs.get("colspan", "1")),
+                        )
+                    )
+                    col_header_index += 1
+                elif cell.kind == NodeKind.TABLE_CELL:
+                    if cell_text in (
+                        "",
+                        "—",
+                        "Text",
+                        "Person",
+                    ) or cell_text.startswith("Flexion:"):
+                        col_index += 1
+                    elif (
+                        cell.contain_node(NodeKind.BOLD)
+                        or len(list(cell.find_html("small"))) > 0
+                    ):  # header in cell
+                        colspan = int(cell.attrs.get("colspan", "1"))
+                        if is_bold_col_header:
+                            for bold_node in cell.find_child(NodeKind.BOLD):
+                                col_headers.append(
+                                    SpanHeader(
+                                        clean_node(wxr, None, bold_node),
+                                        col_header_index,
+                                        colspan,
+                                    )
+                                )
+                        else:
+                            row_header = cell_text
+                        col_header_index += colspan
+                    else:
+                        for form_text in cell_text.splitlines():
+                            form_text = form_text.strip(", ")
+                            form = Form(form=form_text, source=page_tite)
+                            if row_header != "":
+                                form.raw_tags.append(row_header)
+                            for col_header in col_headers:
+                                if (
+                                    col_index >= col_header.index
+                                    and col_index
+                                    < col_header.index + col_header.span
+                                ):
+                                    if col_header.text.endswith("I"):
+                                        form.raw_tags.append(col_header.text)
+                                    else:
+                                        for raw_tag in col_header.text.split():
+                                            form.raw_tags.append(raw_tag)
+                            translate_raw_tags(form)
+                            word_entry.forms.append(form)
+                        col_index += 1
