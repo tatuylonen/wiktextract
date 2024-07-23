842a3646f42243462c9a4e5e6141814316c1ebb8
xxyzz <gitpull@protonmail.com>
2024-07-23 17:22:01 +0800

    [pl] translate some tags and topics data start with "a" and b"
    in page "Pomoc:Skróty używane w Wikisłowniku"

---
+++ src/wiktextract/extractor/pl/tags.py
@@ -0,0 +1,62 @@
+from .models import WordEntry
+
+# Help:Abbreviations used in Wiktionary
+# https://pl.wiktionary.org/wiki/Pomoc:Skróty_używane_w_Wikisłowniku
+# Category:Shortcut templates
+# https://pl.wiktionary.org/wiki/Kategoria:Szablony_skrótów
+TAGS = {
+    "abl.": "ablative",
+    # "akust.": "",
+    "aor.": "aorist",
+    "bezok.": "infinitive",
+    "bezosob.": "impersonal",
+    "bibl.": "Biblical",
+    "blm": "no-plural",
+    "blp": "no-singulative",
+    "Bm": "Bokmål",
+    "bośn.": "Bosnian",
+    "brytań.": "British",
+    "bułg.": "Bulgarian",
+    "bwr.": "Bavarian",
+}
+
+TOPICS = {
+    "adm.": "administration",
+    "agrot.": "agrotechnology",
+    "alch.": "alchemy",
+    "anat.": "anatomy",
+    "antrop.": "anthropology",
+    "arachn.": "arachnology",
+    "archit.": "architecture",
+    "archeol.": "archeology",
+    "astr.": "astronomy",
+    "astrol.": "astrology",
+    "astronaut.": "astronautics",
+    "bank.": "banking",
+    # "bibliot.": "",
+    "biochem.": "biochemistry",
+    "biol.": "biology",
+    # "biur.": "",
+    "bot.": "botany",
+    "bud.": "construction",
+}
+
+
+def translate_raw_tags(data: WordEntry) -> None:
+    raw_tags = []
+    for raw_tag in data.raw_tags:
+        if raw_tag in TAGS and hasattr(data, "tags"):
+            tag = TAGS[raw_tag]
+            if isinstance(tag, str):
+                data.tags.append(tag)
+            elif isinstance(tag, list):
+                data.tags.extend(tag)
+        elif raw_tag in TOPICS and hasattr(data, "topics"):
+            topic = TOPICS[raw_tag]
+            if isinstance(topic, str):
+                data.topics.append(topic)
+            elif isinstance(topic, list):
+                data.topics.extend(topic)
+        else:
+            raw_tags.append(raw_tag)
+    data.raw_tags = raw_tags
