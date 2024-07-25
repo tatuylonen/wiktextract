4fc7dd2cb4eb345f701593a929e5100ad5cc8464
xxyzz <gitpull@protonmail.com>
2024-07-25 15:16:47 +0800

    [pl] extract POS template in italic node

--- src/wiktextract/extractor/pl/pos.py
+++ src/wiktextract/extractor/pl/pos.py
@@ -8,26 +8,37 @@ from .models import Sense, WordEntry
 from .tags import translate_raw_tags
 
 POS_DATA = {
-    "rzeczownik": {"pos": "noun"},
     "czasownik": {"pos": "verb"},
+    # Szablon:szwedzki czasownik frazowy
+    "czasownik frazowy (partikelverb)": {"pos": "verb", "tags": ["phrase"]},
+    "klasyfikator": {"pos": "classifier"},
+    "liczebnik": {"pos": "num"},
+    "międzyrostek": {"pos": "interfix", "tags": ["morpheme"]},
+    "morfem": {"pos": "unknown", "tags": ["morpheme"]},
+    "określnik": {"pos": "det"},
+    "partykuła": {"pos": "particle"},
+    # Szablon:phrasal verb
+    "phrasal verb (czasownik frazowy)": {"pos": "verb", "tags": ["phrase"]},
     "przyimek": {"pos": "prep"},
     "przymiotnik": {"pos": "adj"},
     "przyrostek": {"pos": "suffix", "tags": ["morpheme"]},
-    "wrostek": {"pos": "infix", "tags": ["morpheme"]},
     "przedrostek": {"pos": "prefix", "tags": ["morpheme"]},
     "przysłówek": {"pos": "adv"},
     "przedimek": {"pos": "article"},
-    "klasyfikator": {"pos": "classifier"},
-    "spójnik": {"pos": "conj"},
-    "określnik": {"pos": "det"},
-    "międzyrostek": {"pos": "interfix", "tags": ["morpheme"]},
-    "morfem": {"pos": "unknown", "tags": ["morpheme"]},
-    "wykrzyknik": {"pos": "intj"},
-    "symbol": {"pos": "symbol"},
-    "liczebnik": {"pos": "num"},
-    "partykuła": {"pos": "particle"},
+    "rzeczownik": {"pos": "noun"},
     "skrótowiec": {"pos": "abbrev", "tags": ["abbreviation"]},
+    "spójnik": {"pos": "conj"},
+    "symbol": {"pos": "symbol"},
+    "wrostek": {"pos": "infix", "tags": ["morpheme"]},
+    "wykrzyknik": {"pos": "intj"},
     "zaimek": {"pos": "pron"},
+}
+
+# Category:Proverb Templates
+# https://pl.wiktionary.org/wiki/Kategoria:Szablony_przysłów
+POS_PREFIXES = {
+    "przysłowie": {"pos": "proverb"},
+    "sentencja": {"pos": "phrase"},
 }
 
 
@@ -39,24 +50,48 @@ def extract_pos_section(
 ) -> None:
     for node in level_node.find_child(NodeKind.ITALIC | NodeKind.LIST):
         if node.kind == NodeKind.ITALIC:
-            pos_line_text = clean_node(wxr, None, node)
-            pos_text_list = pos_line_text.split()
-            if len(pos_text_list) == 0:
-                continue
-            pos_text = pos_text_list[0].removesuffix(",")
-            page_data.append(base_data.model_copy(deep=True))
-            if pos_text in POS_DATA:
-                pos_data = POS_DATA[pos_text]
-                page_data[-1].pos = pos_data["pos"]
-                page_data[-1].tags.extend(pos_data.get("tags", []))
-            else:
-                wxr.wtp.debug(
-                    f"Unknown POS: {pos_text}", sortid="extractor/pl/pos"
-                )
-            page_data[-1].pos_text = pos_text
+            process_pos_line_italic_node(wxr, page_data, base_data, node)
         elif node.kind == NodeKind.LIST:
             for list_item in node.find_child(NodeKind.LIST_ITEM):
                 process_gloss_list_item(wxr, page_data[-1], list_item)
+
+
+def process_pos_line_italic_node(
+    wxr: WiktextractContext,
+    page_data: list[WordEntry],
+    base_data: WordEntry,
+    italic_node: WikiNode,
+) -> None:
+    page_data.append(base_data.model_copy(deep=True))
+    for child in italic_node.children:
+        if isinstance(child, TemplateNode):
+            child_text = clean_node(wxr, page_data[-1], child)
+            if child_text in POS_DATA:
+                pos_data = POS_DATA[child_text]
+                page_data[-1].pos = pos_data["pos"]
+                page_data[-1].tags.extend(pos_data.get("tags", []))
+                page_data[-1].pos_text = child_text
+            else:
+                is_pos = False
+                for prefix, pos_data in POS_PREFIXES.items():
+                    if child_text.startswith(prefix):
+                        page_data[-1].pos = pos_data["pos"]
+                        page_data[-1].tags.extend(pos_data.get("tags", []))
+                        page_data[-1].pos_text = child_text
+                        is_pos = True
+                        break
+                if not is_pos:
+                    page_data[-1].raw_tags.append(child_text)
+        elif isinstance(child, str):
+            child = child.strip(", ")
+            for text in child.split():
+                if text in POS_DATA:
+                    pos_data = POS_DATA[text]
+                    page_data[-1].pos = pos_data["pos"]
+                    page_data[-1].tags.extend(pos_data.get("tags", []))
+                    page_data[-1].pos_text = text
+                else:
+                    page_data[-1].raw_tags.append(text)
 
 
 def process_gloss_list_item(
