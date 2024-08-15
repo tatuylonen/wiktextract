c4e7797f0a5f4e3cdcb60073e19a750f39147f09
Kristian Järventaus <kristian@clausal.com>
2024-08-15 09:03:03 +0300

    Forgot to add these .pyi stub files

---
+++ src/wiktextract/table_headers_heuristics_data.pyi
@@ -0,0 +1,4 @@
+# Stub file for overly big .py file that the lsp and linters have trouble with
+
+
+LANGUAGES_WITH_CELLS_AS_HEADERS: dict[str, list[str]]
---
+++ src/wiktextract/taxondata.pyi
@@ -0,0 +1,6 @@
+# Stub file for big data file
+import re
+
+known_species: set[str]
+known_firsts: set[str]
+known_species_re: re.Pattern
