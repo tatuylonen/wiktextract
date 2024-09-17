ab6810b57ecb86b649ba15c92e4fa6ff1a5108be
Kristian Järventaus <kristian@clausal.com>
2024-09-17 13:50:29 +0300

    [simple] Parse etymology

--- src/wiktextract/extractor/simple/etymology.py
+++ src/wiktextract/extractor/simple/etymology.py
@@ -1,8 +1,49 @@
-from wikitextprocessor import WikiNode
+from typing import Optional, Union
 
-from .models import WordEntry
+from wikitextprocessor import WikiNode
+from wikitextprocessor.core import TemplateArgs
+from wiktextract import WiktextractContext
+from wiktextract.clean import clean_value
+from wiktextract.page import clean_node
+from wiktextract.wxr_logging import logger
+
+from .models import TemplateData, WordEntry
+from .parse_utils import ETYMOLOGY_TEMPLATES, PANEL_TEMPLATES
 
 
 def process_etym(
-    child: WikiNode, data: WordEntry
-) -> None: ...
+    wxr: WiktextractContext,
+    etym_nodes: list[Union[str, WikiNode]],
+    target_data: WordEntry,
+) -> None:
+    etym_templates = []
+
+    def post_etym_template_fn(
+        name: str, ht: TemplateArgs, expanded: str
+    ) -> Optional[str]:
+        lname = name.lower().strip()
+        if lname in PANEL_TEMPLATES:
+            return ""
+        if lname in ETYMOLOGY_TEMPLATES:
+            expanded = clean_value(wxr, expanded)
+            new_args = {}
+            for k, v in ht.items():
+                new_args[str(k)] = str(v)
+            tdata = TemplateData(name=name, args=new_args, expansion=expanded)
+            etym_templates.append(tdata)
+        return None
+
+    etym_text = clean_node(
+        wxr, target_data, etym_nodes, post_template_fn=post_etym_template_fn
+    )
+
+    if etym_text:
+        target_data.etymology_text = etym_text
+        if etym_templates:
+            target_data.etymology_templates = etym_templates
+
+        # logprint = []
+        # for t in etym_templates:
+        #     logprint.append(f"=== {t.name}")
+        # lp = '\n'.join(logprint)
+        # logger.info(f"{wxr.wtp.title}\n{lp}")
