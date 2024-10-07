de4e390ee5e04ba81de6447b7bbe5502b9380393
Kristian Järventaus <kristian@clausal.com>
2024-10-07 14:52:37 +0300

    [simple] More comments and minor changes

--- src/wiktextract/extractor/simple/text_utils.py
+++ src/wiktextract/extractor/simple/text_utils.py
@@ -2,13 +2,16 @@ import re
 
 from .section_titles import POS_HEADINGS
 
-# List all the templates registered to POS headings
+# List all the templates registered to POS headings.
 POS_TEMPLATE_NAMES: dict[str, str] = {}
 
 for pos, templates in POS_HEADINGS.items():
     POS_TEMPLATE_NAMES.update({tn: pos for tn in templates["templates"]})
 
-# This is so commonly used that it needs to stop being a magic regex
+# Find digits at end of string, like "Noun 2".
+# This is so commonly used that it needs to stop being a magic regex.
 POS_ENDING_NUMBER_RE = re.compile(r"\s*(\d+)$")
 
+# Use with .strip() and checking for empty strings to eliminate stuff like
+# ", ".
 STRIP_PUNCTUATION = " \t\b,.;:*#-–()[]"
