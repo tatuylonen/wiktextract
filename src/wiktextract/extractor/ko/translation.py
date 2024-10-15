5fe678bdb360c88d07890f10c5eaec7db05590f9
xxyzz <gitpull@protonmail.com>
2024-10-15 14:58:16 +0800

    [ko] extract translation sense data from "외국어" template

--- src/wiktextract/extractor/ko/translation.py
+++ src/wiktextract/extractor/ko/translation.py
@@ -23,15 +23,19 @@ def extract_translation_template(
     t_node: TemplateNode,
 ) -> None:
     # https://ko.wiktionary.org/wiki/틀:외국어
+    sense = clean_node(wxr, None, t_node.template_parameters.get("덧", ""))
     for key in [1, 2]:
         arg_value = t_node.template_parameters.get(key, [])
         parse_arg = wxr.wtp.parse(wxr.wtp.node_to_wikitext(arg_value))
         for list_item in parse_arg.find_child_recursively(NodeKind.LIST_ITEM):
-            extract_translation_list_item(wxr, word_entry, list_item)
+            extract_translation_list_item(wxr, word_entry, list_item, sense)
 
 
 def extract_translation_list_item(
-    wxr: WiktextractContext, word_entry: WordEntry, list_item: WikiNode
+    wxr: WiktextractContext,
+    word_entry: WordEntry,
+    list_item: WikiNode,
+    sense: str,
 ) -> None:
     lang_code = "unknown"
     lang_name = "unknown"
@@ -45,7 +49,12 @@ def extract_translation_list_item(
             word = clean_node(wxr, None, node)
             if word != "":
                 word_entry.translations.append(
-                    Translation(lang=lang_name, lang_code=lang_code, word=word)
+                    Translation(
+                        lang=lang_name,
+                        lang_code=lang_code,
+                        word=word,
+                        sense=sense,
+                    )
                 )
         elif isinstance(node, str) and "(" in node and ")" in node:
             text = ""
