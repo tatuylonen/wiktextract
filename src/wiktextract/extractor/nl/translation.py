87429d6a5438b40a346cd7ed960a4f99c5e1cf73
xxyzz <gitpull@protonmail.com>
2024-10-17 16:56:18 +0800

    [nl] fix check JSON error in page "fijne feestdagen"
    non-standard layout

--- src/wiktextract/extractor/nl/translation.py
+++ src/wiktextract/extractor/nl/translation.py
@@ -56,7 +56,10 @@ def extract_translation_list_item(
     for index, node in enumerate(list_item.children):
         if before_colon and isinstance(node, str) and ":" in node:
             before_colon = False
-            lang_name = clean_node(wxr, None, list_item.children[:index])
+            lang_name = (
+                clean_node(wxr, None, list_item.children[:index])
+                + node[: node.index(":")].strip()
+            )
         elif not before_colon:
             if brackets == 0 and isinstance(node, TemplateNode):
                 if node.template_name == "trad":
