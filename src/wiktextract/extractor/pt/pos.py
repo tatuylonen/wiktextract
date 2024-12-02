c23b79c36eef2e4f0064bb1e88da1bf01be31343
xxyzz <gitpull@protonmail.com>
2024-12-02 16:10:22 +0800

    [pt] extract gloss tag and topic template "escopo" and "escopo2"

--- src/wiktextract/extractor/pt/pos.py
+++ src/wiktextract/extractor/pt/pos.py
@@ -1,4 +1,4 @@
-from wikitextprocessor import LevelNode, NodeKind, WikiNode
+from wikitextprocessor import LevelNode, NodeKind, TemplateNode, WikiNode
 
 from ...page import clean_node
 from ...wxr_context import WiktextractContext
@@ -12,12 +12,14 @@ def extract_pos_section(
     base_data: WordEntry,
     level_node: LevelNode,
     pos_title: str,
+    categories: list[str],
 ) -> None:
     page_data.append(base_data.model_copy(deep=True))
     page_data[-1].pos_title = pos_title
     pos_data = POS_DATA[pos_title]
     page_data[-1].pos = pos_data["pos"]
     page_data[-1].tags.extend(pos_data.get("tags", []))
+    page_data[-1].categories.extend(categories)
 
     for list_index, list_node in level_node.find_child(NodeKind.LIST, True):
         if list_node.sarg.startswith("#") and list_node.sarg.endswith("#"):
@@ -30,9 +32,51 @@ def extract_gloss_list_item(
     word_entry: WordEntry,
     list_item_node: WikiNode,
 ) -> None:
-    gloss_nodes = list(list_item_node.invert_find_child(NodeKind.LIST))
+    gloss_nodes = []
     sense = Sense()
+    for node in list_item_node.children:
+        if isinstance(node, TemplateNode):
+            if node.template_name == "escopo":
+                extract_escopo_template(wxr, sense, node)
+            elif node.template_name == "escopo2":
+                extract_escopo2_template(wxr, sense, node)
+            else:
+                gloss_nodes.append(node)
+        elif isinstance(node, WikiNode) and node.kind == NodeKind.LIST:
+            pass
+        else:
+            gloss_nodes.append(node)
+
     gloss_str = clean_node(wxr, sense, gloss_nodes)
     if len(gloss_str) > 0:
         sense.glosses.append(gloss_str)
         word_entry.senses.append(sense)
+
+
+def extract_escopo_template(
+    wxr: WiktextractContext,
+    sense: Sense,
+    t_node: TemplateNode,
+) -> None:
+    # https://pt.wiktionary.org/wiki/Predefinição:escopo
+    for arg in range(2, 9):
+        if arg not in t_node.template_parameters:
+            break
+        sense.raw_tags.append(
+            clean_node(wxr, None, t_node.template_parameters[arg])
+        )
+    clean_node(wxr, sense, t_node)
+
+
+def extract_escopo2_template(
+    wxr: WiktextractContext,
+    sense: Sense,
+    t_node: TemplateNode,
+) -> None:
+    # https://pt.wiktionary.org/wiki/Predefinição:escopo2
+    for arg in range(1, 4):
+        if arg not in t_node.parameters:
+            break
+        sense.raw_tags.append(
+            clean_node(wxr, None, t_node.template_parameters[arg])
+        )
