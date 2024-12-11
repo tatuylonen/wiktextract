b64f6dc20c67e01bf0bbcbabf27f6cf4bfccb725
xxyzz <gitpull@protonmail.com>
2024-12-11 15:41:08 +0800

    [it] extract gloss lists
    "Appendix" and "Project" namespace pages are added because they are
    used in section title templates "Template:Intestazione voce" and
    "Template:Sezione voce"

---
+++ tests/test_it_gloss.py
@@ -0,0 +1,55 @@
+from unittest import TestCase
+
+from wikitextprocessor import Wtp
+
+from wiktextract.config import WiktionaryConfig
+from wiktextract.extractor.it.page import parse_page
+from wiktextract.wxr_context import WiktextractContext
+
+
+class TestItGloss(TestCase):
+    maxDiff = None
+
+    def setUp(self) -> None:
+        self.wxr = WiktextractContext(
+            Wtp(lang_code="it"),
+            WiktionaryConfig(
+                dump_file_lang_code="it", capture_language_codes=None
+            ),
+        )
+
+    def test_gloss_list(self):
+        self.wxr.wtp.add_page("Template:-it-", 10, "Italiano")
+        self.wxr.wtp.add_page(
+            "Template:Term",
+            10,
+            "<small>(<i>[[mammalogia]]</i>)</small>[[Categoria:Mammalogia-IT]]",
+        )
+        data = parse_page(
+            self.wxr,
+            "cane",
+            """== {{-it-}} ==
+===[[Image:Open_book_01.svg|30px|]]''[[sostantivo|Sostantivo]]''===
+[[Categoria:Sostantivi in italiano]]
+
+# {{Term|mammalogia|it}} [[animale]]""",
+        )
+        self.assertEqual(
+            data,
+            [
+                {
+                    "categories": ["Sostantivi in italiano"],
+                    "lang": "Italiano",
+                    "lang_code": "it",
+                    "word": "cane",
+                    "pos": "noun",
+                    "pos_title": "Sostantivo",
+                    "senses": [
+                        {
+                            "glosses": ["(mammalogia) animale"],
+                            "categories": ["Mammalogia-IT"],
+                        }
+                    ],
+                }
+            ],
+        )
