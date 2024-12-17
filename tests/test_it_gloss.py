b7ab69f1ec2eded41ab5968c9420b348692f021d
xxyzz <gitpull@protonmail.com>
2024-12-17 15:34:57 +0800

    [it] handle pos subsection templates

--- tests/test_it_gloss.py
+++ tests/test_it_gloss.py
@@ -54,3 +54,107 @@ class TestItGloss(TestCase):
                 }
             ],
         )
+
+    def test_double_pos_subsection_templates(self):
+        self.wxr.wtp.add_page("Template:-la-", 10, "Latino")
+        self.wxr.wtp.add_page(
+            "Template:Intransitivo",
+            10,
+            """====[[intransitivo|Intransitivo]]====
+[[Categoria:Verbi intransitivi_in_latino]]""",
+        )
+        self.wxr.wtp.add_page(
+            "Template:Deponente",
+            10,
+            """====[[deponente|Deponente]]====
+[[Categoria:Verbi deponenti_in_latino]]""",
+        )
+        data = parse_page(
+            self.wxr,
+            "aboriscor",
+            """== {{-la-}} ==
+===[[Image:Open_book_01.svg|30px|]]''[[verbo|Verbo]]''===
+[[Categoria:Verbi in latino]]
+{{Intransitivo|la}}
+{{Deponente|la}}
+'''ăbŏriscor'''
+
+# [[venir]] [[meno]]""",
+        )
+        self.assertEqual(
+            data,
+            [
+                {
+                    "lang": "Latino",
+                    "lang_code": "la",
+                    "word": "aboriscor",
+                    "pos": "verb",
+                    "pos_title": "Verbo",
+                    "categories": [
+                        "Verbi in latino",
+                        "Verbi intransitivi_in_latino",
+                        "Verbi deponenti_in_latino",
+                    ],
+                    "senses": [{"glosses": ["venir meno"]}],
+                    "raw_tags": ["Intransitivo", "Deponente"],
+                }
+            ],
+        )
+
+    def test_subsecton_template_add_new_word_entry(self):
+        self.wxr.wtp.add_page("Template:-it-", 10, "Italiano")
+        self.wxr.wtp.add_page(
+            "Template:Ausiliare",
+            10,
+            """====[[ausiliare|Ausiliare]]====
+[[Categoria:Verbi ausiliari_in_italiano]]""",
+        )
+        self.wxr.wtp.add_page(
+            "Template:Intransitivo",
+            10,
+            """====[[intransitivo|Intransitivo]]====
+[[Categoria:Verbi intransitivi_in_latino]]""",
+        )
+        data = parse_page(
+            self.wxr,
+            "essere",
+            """== {{-it-}} ==
+===[[Image:Open_book_01.svg|30px|]]''[[verbo|Verbo]]''===
+[[Categoria:Verbi in italiano]]
+{{Ausiliare|it}}
+# serve per la coniugazione
+
+{{Intransitivo|it}}
+# Questo verbo serve per dire""",
+        )
+        self.assertEqual(
+            data,
+            [
+                {
+                    "lang": "Italiano",
+                    "lang_code": "it",
+                    "word": "essere",
+                    "pos": "verb",
+                    "pos_title": "Verbo",
+                    "categories": [
+                        "Verbi in italiano",
+                        "Verbi ausiliari_in_italiano",
+                    ],
+                    "senses": [{"glosses": ["serve per la coniugazione"]}],
+                    "raw_tags": ["Ausiliare"],
+                },
+                {
+                    "lang": "Italiano",
+                    "lang_code": "it",
+                    "word": "essere",
+                    "pos": "verb",
+                    "pos_title": "Verbo",
+                    "categories": [
+                        "Verbi in italiano",
+                        "Verbi intransitivi_in_latino",
+                    ],
+                    "senses": [{"glosses": ["Questo verbo serve per dire"]}],
+                    "raw_tags": ["Intransitivo"],
+                },
+            ],
+        )
