b4cfa6239e0843c0c53f3e81aecd7fdd7c876b62
xxyzz <gitpull@protonmail.com>
2024-12-20 11:09:12 +0800

    [it] translate "it-conj" template tags

--- src/wiktextract/extractor/it/tags.py
+++ src/wiktextract/extractor/it/tags.py
@@ -10,6 +10,27 @@ TABLE_TAGS = {
     "femminile": "feminine",
     # https://it.wiktionary.org/wiki/Template:It-decl-agg2
     "m e f": ["masculine", "feminine"],
+    # https://it.wiktionary.org/wiki/Template:It-conj
+    "infinito": "infinitive",
+    "verbo ausiliare": "auxiliary",
+    "gerundio": "gerund",
+    "participio presente": ["present", "participle"],
+    "participio passato": ["past", "participle"],
+    "prima": "first-person",
+    "seconda": "second-person",
+    "terza": "third-person",
+    "presente": "present",
+    "imperfetto": "imperfect",
+    "passato remoto": "past-remote",
+    "futuro": "future",
+    "passato prossimo": ["past", "perfect"],
+    "trapassato prossimo": ["pluperfect", "past", "perfect"],
+    "trapassato remoto": ["historic", "past-remote"],
+    "futuro anteriore": ["future", "perfect"],
+    "passato": "past",
+    "trapassato": ["past", "perfect"],
+    "imperativo": "imperative",
+    "riflessivo pronominale": ["reflexive", "pronominal"],
 }
 
 FORM_LINE_TEMPLATE_TAGS = {
