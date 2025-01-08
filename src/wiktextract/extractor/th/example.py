0a34c138d5855fd33968425067f4837ffb4dfe18
xxyzz <gitpull@protonmail.com>
2025-01-08 15:12:13 +0800

    [th] extract "zh-x" example template

--- src/wiktextract/extractor/th/example.py
+++ src/wiktextract/extractor/th/example.py
@@ -1,8 +1,11 @@
-from wikitextprocessor import NodeKind, TemplateNode, WikiNode
+import re
+
+from wikitextprocessor import HTMLNode, NodeKind, TemplateNode, WikiNode
 
 from ...page import clean_node
 from ...wxr_context import WiktextractContext
 from .models import Example, Sense
+from .tags import translate_raw_tags
 
 
 def extract_example_list_item(
@@ -12,6 +15,8 @@ def extract_example_list_item(
         if isinstance(node, TemplateNode):
             if node.template_name in ["ux", "usex", "ko-usex"]:
                 extract_ux_template(wxr, sense, node)
+            elif node.template_name in ["zh-x", "zh-usex"]:
+                extract_template_zh_x(wxr, sense, node)
 
 
 def extract_ux_template(
@@ -42,6 +47,86 @@ def extract_ux_template(
         wxr, None, t_node.template_parameters.get("ref", "")
     )
     if e_data.text != "":
+        translate_raw_tags(e_data)
         sense.examples.append(e_data)
         for link_node in expanded_node.find_child(NodeKind.LINK):
             clean_node(wxr, sense, link_node)
+
+
+def extract_template_zh_x(
+    wxr: WiktextractContext,
+    sense: Sense,
+    t_node: TemplateNode,
+) -> None:
+    expanded_node = wxr.wtp.parse(
+        wxr.wtp.node_to_wikitext(t_node), expand_all=True
+    )
+    examples = []
+    for dl_tag in expanded_node.find_html("dl"):
+        examples.extend(extract_zh_x_dl_tag(wxr, dl_tag))
+    if len(examples) == 0:
+        examples.extend(extract_zh_x_no_dl_tag(wxr, expanded_node))
+
+    translation = clean_node(wxr, None, t_node.template_parameters.get(2, ""))
+    for e_data in examples:
+        e_data.translation = translation
+        translate_raw_tags(e_data)
+
+    for link_node in expanded_node.find_child(NodeKind.LINK):
+        clean_node(wxr, sense, link_node)
+
+    sense.examples.extend(examples)
+
+
+def extract_zh_x_dl_tag(
+    wxr: WiktextractContext, dl_tag: HTMLNode
+) -> list[Example]:
+    examples = []
+    for span_tag in dl_tag.find_html("span"):
+        if "lang" in span_tag.attrs:
+            e_text = clean_node(wxr, None, span_tag)
+            if e_text != "":
+                examples.append(Example(text=e_text))
+        else:
+            raw_tags = clean_node(wxr, None, span_tag).strip("[] ")
+            for raw_tag in re.split(r", | and ", raw_tags):
+                raw_tag = raw_tag.strip()
+                if raw_tag != "" and len(examples) > 0:
+                    examples[-1].raw_tags.append(raw_tag)
+    for dd_tag in dl_tag.find_html("dd"):
+        for span_tag in dd_tag.find_html("span"):
+            if "Latn" in span_tag.attrs.get("lang", ""):
+                roman = clean_node(wxr, None, span_tag)
+                for e_data in examples:
+                    e_data.roman = roman
+            else:
+                raw_tag = clean_node(wxr, None, span_tag).strip("[] ")
+                if raw_tag != "":
+                    for e_data in examples:
+                        e_data.raw_tags.append(raw_tag)
+    return examples
+
+
+def extract_zh_x_no_dl_tag(
+    wxr: WiktextractContext, expanded_node: WikiNode
+) -> list[Example]:
+    examples = []
+    for span_tag in expanded_node.find_html("span"):
+        lang = span_tag.attrs.get("lang", "")
+        match lang:
+            case "zh-Latn":
+                roman = clean_node(wxr, None, span_tag)
+                for e_data in examples:
+                    e_data.roman = roman
+            case "zh-Hant" | "zh-Hans":
+                e_text = clean_node(wxr, None, span_tag)
+                example = Example(text=e_text)
+                example.tags.append(
+                    "Traditional Chinese"
+                    if lang == "zh-Hant"
+                    else "Simplified Chinese"
+                )
+                if example.text != "":
+                    examples.append(example)
+
+    return examples
--- tests/test_th_example.py
+++ tests/test_th_example.py
@@ -48,3 +48,125 @@ class TestThExample(TestCase):
                 ],
             },
         )
+
+    def test_zh_x_one_line(self):
+        self.wxr.wtp.add_page(
+            "แม่แบบ:zh-x",
+            10,
+            """<span lang="zh-Hant" class="Hant"><b>大家</b>[[都#Chinese|都]][[還#Chinese|還]][[行#Chinese|行]]。</span><span lang="zh-Hani" class="Hani">／</span><span lang="zh-Hans" class="Hans"><b>大家</b>[[都#Chinese|都]][[还#Chinese|还]][[行#Chinese|行]]。</span>&nbsp; ―&nbsp; <span lang="zh-Latn" style="color:#404D52"><i><b>Dàjiā</b> dōu hái xíng.</i></span>&nbsp; ―&nbsp; '''ทุกคน'''ยังสบายดี[[Category:ศัพท์ภาษาจีนกลางที่มีตัวอย่างการใช้]]""",
+        )
+        page_data = parse_page(
+            self.wxr,
+            "大家",
+            """== ภาษาจีน ==
+=== คำสรรพนาม ===
+{{zh-pronoun}}
+
+# [[ทุกคน]]; [[พวกเรา]]ทุกคน
+#: {{zh-x|大家 都 還 行。|'''ทุกคน'''ยังสบายดี}}""",
+        )
+        self.assertEqual(
+            page_data[0]["senses"][0],
+            {
+                "categories": ["ศัพท์ภาษาจีนกลางที่มีตัวอย่างการใช้"],
+                "glosses": ["ทุกคน; พวกเราทุกคน"],
+                "examples": [
+                    {
+                        "text": "大家都還行。",
+                        "roman": "Dàjiā dōu hái xíng.",
+                        "translation": "ทุกคนยังสบายดี",
+                        "tags": ["Traditional Chinese"],
+                    },
+                    {
+                        "text": "大家都还行。",
+                        "roman": "Dàjiā dōu hái xíng.",
+                        "translation": "ทุกคนยังสบายดี",
+                        "tags": ["Simplified Chinese"],
+                    },
+                ],
+            },
+        )
+
+    def test_hant_and_hans_same_text(self):
+        self.wxr.wtp.add_page(
+            "แม่แบบ:zh-x",
+            10,
+            """<dl class="zhusex"><span lang="zh-Hant" class="Hant"><b>大家</b>[[都#Chinese|都]][[很#Chinese|很]][[尊敬#Chinese|尊敬]][[他#Chinese|他]]。</span> <span style="color:darkgreen; font-size:x-small;">&#91;[[w:Standard Chinese|MSC]], <i>[[w:Traditional Chinese|trad.]]</i> and <i>[[w:Simplified Chinese|simp.]]</i>&#93;</span><dd><span lang="zh-Latn" style="color:#404D52"><i><b>Dàjiā</b> dōu hěn zūnjìng tā.</i></span> <span style="color:darkgreen; font-size:x-small;">&#91;Pinyin&#93;</span></dd><dd>'''ทุกคน'''ต่างเคารพเขามาก</dd></dl>[[Category:ศัพท์ภาษาจีนกลางที่มีตัวอย่างการใช้]]""",
+        )
+        page_data = parse_page(
+            self.wxr,
+            "大家",
+            """== ภาษาจีน ==
+=== คำสรรพนาม ===
+{{zh-pronoun}}
+
+# [[ทุกคน]]; [[พวกเรา]]ทุกคน
+#: {{zh-x|大家 都 很 尊敬 他。|'''ทุกคน'''ต่างเคารพเขามาก}}""",
+        )
+        self.assertEqual(
+            page_data[0]["senses"][0],
+            {
+                "categories": ["ศัพท์ภาษาจีนกลางที่มีตัวอย่างการใช้"],
+                "glosses": ["ทุกคน; พวกเราทุกคน"],
+                "examples": [
+                    {
+                        "text": "大家都很尊敬他。",
+                        "roman": "Dàjiā dōu hěn zūnjìng tā.",
+                        "translation": "ทุกคนต่างเคารพเขามาก",
+                        "tags": [
+                            "Modern Standard Chinese",
+                            "Traditional Chinese",
+                            "Simplified Chinese",
+                            "Pinyin",
+                        ],
+                    }
+                ],
+            },
+        )
+
+    def test_zh_x_two_text_lines(self):
+        self.wxr.wtp.add_page(
+            "แม่แบบ:zh-x",
+            10,
+            """<dl class="zhusex"><span lang="zh-Hant" class="Hant">[[請#Chinese|請]]<b>大家</b>[[保持#Chinese|保持]][[安靜#Chinese|安靜]]。</span> <span style="color:darkgreen; font-size:x-small;">&#91;[[w:Standard Chinese|MSC]], <i>[[w:Traditional Chinese|trad.]]</i>&#93;</span><br><span lang="zh-Hans" class="Hans">[[请#Chinese|请]]<b>大家</b>[[保持#Chinese|保持]][[安静#Chinese|安静]]。</span> <span style="color:darkgreen; font-size:x-small;">&#91;[[w:Standard Chinese|MSC]], <i>[[w:Simplified Chinese|simp.]]</i>&#93;</span><dd><span lang="zh-Latn" style="color:#404D52"><i>Qǐng <b>dàjiā</b> bǎochí ānjìng.</i></span> <span style="color:darkgreen; font-size:x-small;">&#91;Pinyin&#93;</span></dd><dd>'''ทุกคน'''กรุณาเงียบ</dd></dl>[[Category:ศัพท์ภาษาจีนกลางที่มีตัวอย่างการใช้]]
+""",
+        )
+        page_data = parse_page(
+            self.wxr,
+            "大家",
+            """== ภาษาจีน ==
+=== คำสรรพนาม ===
+{{zh-pronoun}}
+
+# [[ทุกคน]]; [[พวกเรา]]ทุกคน
+#: {{zh-x|請 大家 保持 安靜。|'''ทุกคน'''กรุณาเงียบ}}""",
+        )
+        self.assertEqual(
+            page_data[0]["senses"][0],
+            {
+                "categories": ["ศัพท์ภาษาจีนกลางที่มีตัวอย่างการใช้"],
+                "glosses": ["ทุกคน; พวกเราทุกคน"],
+                "examples": [
+                    {
+                        "text": "請大家保持安靜。",
+                        "roman": "Qǐng dàjiā bǎochí ānjìng.",
+                        "translation": "ทุกคนกรุณาเงียบ",
+                        "tags": [
+                            "Modern Standard Chinese",
+                            "Traditional Chinese",
+                            "Pinyin",
+                        ],
+                    },
+                    {
+                        "text": "请大家保持安静。",
+                        "roman": "Qǐng dàjiā bǎochí ānjìng.",
+                        "translation": "ทุกคนกรุณาเงียบ",
+                        "tags": [
+                            "Modern Standard Chinese",
+                            "Simplified Chinese",
+                            "Pinyin",
+                        ],
+                    },
+                ],
+            },
+        )
