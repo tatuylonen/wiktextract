04739f4ee6283ae4c8d1b7869d66eb38252180e8
xxyzz <gitpull@protonmail.com>
2025-01-23 15:21:28 +0800

    [ku] extract gloss lists

---
+++ src/wiktextract/extractor/ku/models.py
@@ -0,0 +1,30 @@
+from pydantic import BaseModel, ConfigDict, Field
+
+
+class KurdishBaseModel(BaseModel):
+    model_config = ConfigDict(
+        extra="forbid",
+        strict=True,
+        validate_assignment=True,
+        validate_default=True,
+    )
+
+
+class Sense(KurdishBaseModel):
+    glosses: list[str] = []
+    tags: list[str] = []
+    raw_tags: list[str] = []
+    categories: list[str] = []
+
+
+class WordEntry(KurdishBaseModel):
+    model_config = ConfigDict(title="Kurdish Wiktionary")
+    word: str = Field(description="Word string")
+    lang_code: str = Field(description="Wiktionary language code")
+    lang: str = Field(description="Localized language name")
+    pos: str = Field(description="Part of speech type")
+    pos_title: str = ""
+    senses: list[Sense] = []
+    categories: list[str] = []
+    tags: list[str] = []
+    raw_tags: list[str] = []
---
+++ tests/test_ku_gloss.py
@@ -0,0 +1,59 @@
+from unittest import TestCase
+
+from wikitextprocessor import Wtp
+
+from wiktextract.config import WiktionaryConfig
+from wiktextract.extractor.ku.page import parse_page
+from wiktextract.wxr_context import WiktextractContext
+
+
+class TestKuGloss(TestCase):
+    maxDiff = None
+
+    def setUp(self) -> None:
+        self.wxr = WiktextractContext(
+            Wtp(lang_code="ku"),
+            WiktionaryConfig(
+                dump_file_lang_code="ku", capture_language_codes=None
+            ),
+        )
+
+    def tearDown(self):
+        self.wxr.wtp.close_db_conn()
+
+    def test_f_template(self):
+        self.wxr.wtp.add_page(
+            "Şablon:ziman",
+            10,
+            """<span class="sectionlangue" id="ku">[[kurmancî|Kurmancî]][[Kategorî:Kurmancî]]</span>""",
+        )
+        self.wxr.wtp.add_page(
+            "Şablon:f",
+            10,
+            """<i><span class="ib-brac">(</span><span class="ib-content">[[guhandar]][[Category:Guhandar bi kurmancî|A]]</span><span class="ib-brac">)</span></i>""",
+        )
+        page_data = parse_page(
+            self.wxr,
+            "kûçik",
+            """== {{ziman|ku}} ==
+=== Navdêr ===
+# {{f|ku|guhandar}} [[heywan|Heywanek]]""",
+        )
+        self.assertEqual(
+            page_data[0],
+            {
+                "categories": ["Kurmancî"],
+                "lang": "Kurmancî",
+                "lang_code": "ku",
+                "word": "kûçik",
+                "pos": "noun",
+                "pos_title": "Navdêr",
+                "senses": [
+                    {
+                        "categories": ["Guhandar bi kurmancî"],
+                        "glosses": ["Heywanek"],
+                        "raw_tags": ["guhandar"],
+                    }
+                ],
+            },
+        )
