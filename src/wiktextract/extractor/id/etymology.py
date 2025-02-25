6804880443583efa976778897812d0be63861c1e
xxyzz <gitpull@protonmail.com>
2025-02-25 17:06:27 +0800

    [id] extract etymology section

---
+++ src/wiktextract/extractor/id/etymology.py
@@ -0,0 +1,25 @@
+from wikitextprocessor.parser import LEVEL_KIND_FLAGS, LevelNode, NodeKind
+
+from ...page import clean_node
+from ...wxr_context import WiktextractContext
+from .models import WordEntry
+
+
+def extract_etymology_section(
+    wxr: WiktextractContext,
+    word_entry: WordEntry,
+    level_node: LevelNode,
+) -> None:
+    for list_node in level_node.find_child(NodeKind.LIST):
+        for list_item in list_node.find_child(NodeKind.LIST_ITEM):
+            e_str = clean_node(wxr, word_entry, list_item.children)
+            if e_str != "":
+                word_entry.etymology_texts.append(e_str)
+    if len(word_entry.etymology_texts) == 0:
+        e_str = clean_node(
+            wxr,
+            word_entry,
+            list(level_node.invert_find_child(LEVEL_KIND_FLAGS)),
+        )
+        if e_str != "":
+            word_entry.etymology_texts.append(e_str)
