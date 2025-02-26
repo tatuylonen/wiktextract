eeb66be39b423a2cf564bf18e7a4bc829f11663c
xxyzz <gitpull@protonmail.com>
2025-02-26 16:15:43 +0800

    [id] extract translation section

--- src/wiktextract/extractor/id/page.py
+++ src/wiktextract/extractor/id/page.py
@@ -9,6 +9,7 @@ from .etymology import extract_etymology_section
 from .models import Sense, WordEntry
 from .pos import extract_pos_section
 from .section_titles import POS_DATA
+from .translation import extract_translation_section
 
 
 def parse_section(
@@ -23,6 +24,10 @@ def parse_section(
         extract_pos_section(wxr, page_data, base_data, level_node, title_text)
     elif title_text == "Etimologi":
         extract_etymology_section(
+            wxr, page_data[-1] if len(page_data) > 0 else base_data, level_node
+        )
+    elif title_text == "Terjemahan":
+        extract_translation_section(
             wxr, page_data[-1] if len(page_data) > 0 else base_data, level_node
         )
 
