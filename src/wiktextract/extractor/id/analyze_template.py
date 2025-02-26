aee75042b97f3faf5ba330bca2d84d41c8896eb1
xxyzz <gitpull@protonmail.com>
2025-02-26 11:25:20 +0800

    [id] pre-expand more POS and language section templates

--- src/wiktextract/extractor/id/analyze_template.py
+++ src/wiktextract/extractor/id/analyze_template.py
@@ -1,48 +1,22 @@
 from wikitextprocessor import Page, Wtp
 
-# https://id.wiktionary.org/wiki/Wikikamus:Penjelasan_tataletak_entri
-# https://id.wiktionary.org/wiki/Kategori:Templat_kelas_kata
-# https://id.wiktionary.org/wiki/Kategori:Templat_umum
 SECTION_TITLE_TEMPLATES = {
-    "Templat:-adj-",
-    "Templat:-adv-",
-    "Templat:-akronim-",
-    "Templat:-art-",
-    "Templat:-GantiDenganKelasKata-",
-    "Templat:-gol-",
-    "Templat:-inter-",
-    "Templat:-intero-",
-    "Templat:-kon-",
-    "Templat:-nom-",
-    "Templat:-nom-ulang-",
-    "Templat:-num-",
-    "Templat:-part-",
-    "Templat:-prep-",
-    "Templat:-pronom-",
-    "Templat:-sub-",
-    "Templat:-verb-",
-    "Templat:=adj=",
-    "Templat:=adv=",
-    "Templat:=inter=",
-    "Templat:=intero=",
-    "Templat:=kon=",
-    "Templat:=nom=",
-    "Templat:=num=",
-    "Templat:=p=",
-    "Templat:=part=",
-    "Templat:=prep=",
-    "Templat:=pron=",
-    "Templat:=pronom=",
-    "Templat:=verb=",
     "Templat:ulang",
-    "Templat:-sdd-",
+    "Templat:zh-num",
+    "Templat:zh-inter",
 }
 
 
 def analyze_template(wtp: Wtp, page: Page) -> tuple[set[str], bool]:
+    # https://id.wiktionary.org/wiki/Wikikamus:Penjelasan_tataletak_entri
+    # https://id.wiktionary.org/wiki/Kategori:Templat_kelas_kata
+    # https://id.wiktionary.org/wiki/Kategori:Templat_umum
+    # https://id.wiktionary.org/wiki/Kategori:Templat_bahasa
     return (
         set(),
         page.title in SECTION_TITLE_TEMPLATES
+        or (page.title.startswith("Templat:-") and page.title.endswith("-"))
+        or (page.title.startswith("Templat:=") and page.title.endswith("="))
         or page.title.startswith(
             (
                 "Templat:imbuhan ",
@@ -52,5 +26,7 @@ def analyze_template(wtp: Wtp, page: Page) -> tuple[set[str], bool]:
                 "Templat:nomina ",
             )
         )
-        or page.title.endswith(("proper noun", "-nm")),
+        or page.title.endswith(
+            ("proper noun", "-aj", "-av", "-nm", "-vb", "-prnc")
+        ),
     )
