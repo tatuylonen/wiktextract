0084c134041ffd1d562d510cf7fa3c98b3e4c869
xxyzz <gitpull@protonmail.com>
2025-04-08 13:54:43 +0800

    [ru] extract "мест ru *" and "числ ru *" table templates

--- src/wiktextract/extractor/ru/inflection.py
+++ src/wiktextract/extractor/ru/inflection.py
@@ -16,92 +16,79 @@ class TableHeader:
     span: int
 
 
-def parse_adj_forms_table(
-    wxr: WiktextractContext,
-    word_entry: WordEntry,
-    expanded_template: WikiNode,
+def parse_html_forms_table(
+    wxr: WiktextractContext, word_entry: WordEntry, table_tag: HTMLNode
 ):
     # HTML table
     # https://ru.wiktionary.org/wiki/Шаблон:прил
-    for table_element in expanded_template.find_html("table"):
-        column_headers = []
-        row_headers = []
-        td_rowspan = defaultdict(int)
-        for tr_element in table_element.find_html("tr"):
-            if len(list(tr_element.find_html("td"))) == 0:
-                # all header
-                current_index = 0
-                for th_element in tr_element.find_html("th"):
+    column_headers = []
+    row_headers = []
+    td_rowspan = defaultdict(int)
+    for tr_element in table_tag.find_html("tr"):
+        if len(list(tr_element.find_html("td"))) == 0:
+            # all header
+            current_index = 0
+            for th_element in tr_element.find_html("th"):
+                header_text = ""
+                for header_link in th_element.find_child(NodeKind.LINK):
+                    header_text = clean_node(wxr, None, header_link)
+                if header_text == "падеж":
+                    continue  # ignore top left corner header
+                header_span = int(th_element.attrs.get("colspan", "1"))
+                column_headers.append(
+                    TableHeader(header_text, current_index, header_span)
+                )
+                current_index += header_span
+        else:
+            col_index = 0
+            has_rowspan = False
+            for td_element in tr_element.find_html("td"):
+                if td_element.attrs.get("bgcolor") == "#EEF9FF":
+                    # this is a td tag but contains header text
                     header_text = ""
-                    for header_link in th_element.find_child(NodeKind.LINK):
+                    for header_link in td_element.find_child(NodeKind.LINK):
                         header_text = clean_node(wxr, None, header_link)
-                    if header_text == "падеж":
-                        continue  # ignore top left corner header
-                    header_span = int(th_element.attrs.get("colspan", "1"))
-                    column_headers.append(
-                        TableHeader(header_text, current_index, header_span)
-                    )
-                    current_index += header_span
-            else:
-                col_index = 0
-                has_rowspan = False
-                for td_element in tr_element.find_html("td"):
-                    if td_element.attrs.get("bgcolor") == "#EEF9FF":
-                        # this is a td tag but contains header text
-                        header_text = ""
-                        for header_link in td_element.find_child(NodeKind.LINK):
-                            header_text = clean_node(wxr, None, header_link)
-                        header_span = int(td_element.attrs.get("rowspan", "1"))
-                        row_headers.append(
-                            TableHeader(header_text, 0, header_span)
-                        )
-                        continue
-                    if "rowspan" in td_element.attrs:
-                        td_rowspan[col_index] = (
-                            int(td_element.attrs["rowspan"]) - 1
-                        )
-                        has_rowspan = True
-                    elif not has_rowspan:
-                        for rowspan_index, rowspan_value in td_rowspan.items():
-                            if rowspan_value > 0 and col_index == rowspan_index:
-                                col_index += 1
-                                td_rowspan[rowspan_index] -= 1
-                    td_text = clean_node(wxr, None, td_element)
-                    for line in td_text.split():
-                        form = Form(form=line)
-                        for col_header in column_headers:
-                            if (
-                                col_index >= col_header.start_index
-                                and col_index
-                                < col_header.start_index + col_header.span
-                            ):
-                                form.raw_tags.append(col_header.text)
-                        form.raw_tags.extend([h.text for h in row_headers])
-                        if len(form.form) > 0:
-                            translate_raw_tags(form)
-                            word_entry.forms.append(form)
-                    col_index += 1
+                    header_span = int(td_element.attrs.get("rowspan", "1"))
+                    row_headers.append(TableHeader(header_text, 0, header_span))
+                    continue
+                if "rowspan" in td_element.attrs:
+                    td_rowspan[col_index] = int(td_element.attrs["rowspan"]) - 1
+                    has_rowspan = True
+                elif not has_rowspan:
+                    for rowspan_index, rowspan_value in td_rowspan.items():
+                        if rowspan_value > 0 and col_index == rowspan_index:
+                            col_index += 1
+                            td_rowspan[rowspan_index] -= 1
+                td_text = clean_node(wxr, None, td_element)
+                for line in td_text.split():
+                    form = Form(form=line)
+                    for col_header in column_headers:
+                        if (
+                            col_index >= col_header.start_index
+                            and col_index
+                            < col_header.start_index + col_header.span
+                        ):
+                            form.raw_tags.append(col_header.text)
+                    form.raw_tags.extend([h.text for h in row_headers])
+                    if len(form.form) > 0:
+                        translate_raw_tags(form)
+                        word_entry.forms.append(form)
+                col_index += 1
 
-            updated_row_headers = []
-            for row_header in row_headers:
-                if row_header.span > 1:
-                    row_header.span -= 1
-                    updated_row_headers.append(row_header)
-            row_headers = updated_row_headers
+        updated_row_headers = []
+        for row_header in row_headers:
+            if row_header.span > 1:
+                row_header.span -= 1
+                updated_row_headers.append(row_header)
+        row_headers = updated_row_headers
 
 
 def parse_wikitext_forms_table(
-    wxr: WiktextractContext,
-    word_entry: WordEntry,
-    expanded_template: WikiNode,
+    wxr: WiktextractContext, word_entry: WordEntry, table_node: WikiNode
 ) -> None:
     # https://ru.wiktionary.org/wiki/Шаблон:сущ-ru
     # Шаблон:inflection сущ ru
     # Шаблон:Гл-блок
-    table_nodes = list(expanded_template.find_child(NodeKind.TABLE))
-    if len(table_nodes) == 0:
-        return
-    table_node = table_nodes[0]
     column_headers = []
     for table_row in table_node.find_child(NodeKind.TABLE_ROW):
         row_headers = []
@@ -169,8 +156,6 @@ def parse_wikitext_forms_table(
                         td_index,
                     )
 
-    clean_node(wxr, word_entry, expanded_template)  # add category links
-
 
 def add_form_data(
     word_entry: WordEntry,
@@ -181,7 +166,7 @@ def add_form_data(
 ) -> None:
     form = Form(form=form_text.strip(" /"))
     form.raw_tags.extend(row_headers)
-    if col_index < len(col_headers):
+    if col_index < len(col_headers) and col_headers[col_index] != "":
         form.raw_tags.append(col_headers[col_index])
     if len(form.form) > 0 and form.form != "—":
         translate_raw_tags(form)
