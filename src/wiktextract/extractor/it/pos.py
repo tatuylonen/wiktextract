887c100532edf88995a335ddfff1fa875ac77ab7
xxyzz <gitpull@protonmail.com>
2025-05-07 16:11:14 +0800

    [it] fix POS header data added to wrong `WordEntry` bug
    page "piangere", only the first header has appendix forms page link

--- src/wiktextract/extractor/it/pos.py
+++ src/wiktextract/extractor/it/pos.py
@@ -57,7 +57,7 @@ def extract_pos_section(
     pos_title: str,
 ) -> None:
     add_new_pos_data(wxr, page_data, base_data, level_node, pos_title)
-    first_gloss_list_index = len(level_node.children)
+    last_gloss_list_index = 0
     for index, node in enumerate(level_node.children):
         if (
             isinstance(node, WikiNode)
@@ -67,8 +67,12 @@ def extract_pos_section(
         ):
             for list_item in node.find_child(NodeKind.LIST_ITEM):
                 extract_gloss_list_item(wxr, page_data[-1], list_item)
-            if index < first_gloss_list_index:
-                first_gloss_list_index = index
+            extract_tag_form_line_nodes(
+                wxr,
+                page_data[-1],
+                level_node.children[last_gloss_list_index:index],
+            )
+            last_gloss_list_index = index + 1
         elif (
             isinstance(node, TemplateNode)
             and node.template_name in POS_SUBSECTION_TEMPLATES
@@ -79,10 +83,6 @@ def extract_pos_section(
                 )
             raw_tag = clean_node(wxr, page_data[-1], node).strip("= \n")
             page_data[-1].raw_tags.append(raw_tag)
-
-    extract_tag_form_line_nodes(
-        wxr, page_data[-1], level_node.children[:first_gloss_list_index]
-    )
 
 
 def extract_gloss_list_item(
