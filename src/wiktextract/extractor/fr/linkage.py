ef1d9b5707e9fd7ed78105b712f94ae4b8d27e81
xxyzz <gitpull@protonmail.com>
2025-09-25 10:10:47 +0800

    [fr] extract bold sense node in linkage section

--- src/wiktextract/extractor/fr/linkage.py
+++ src/wiktextract/extractor/fr/linkage.py
@@ -1,6 +1,6 @@
 import re
 
-from wikitextprocessor import NodeKind, TemplateNode, WikiNode
+from wikitextprocessor import LevelNode, NodeKind, TemplateNode, WikiNode
 
 from ...page import clean_node
 from ...wxr_context import WiktextractContext
@@ -13,7 +13,7 @@ from .tags import translate_raw_tags
 def extract_linkage(
     wxr: WiktextractContext,
     page_data: list[WordEntry],
-    level_node: WikiNode,
+    level_node: LevelNode,
     section_type: str,
 ) -> None:
     if section_type == "dérivés autres langues":
@@ -26,9 +26,9 @@ def extract_linkage(
                     if data.lang_code == page_data[-1].lang_code:
                         data.anagrams.extend(anagram_list)
     else:
-        process_linkage_list(
+        extract_linkage_section(
             wxr,
-            page_data,
+            page_data[-1],
             level_node,
             LINKAGE_SECTIONS[section_type],
             LINKAGE_TAGS.get(section_type, []),
@@ -36,9 +36,7 @@ def extract_linkage(
 
 
 def process_derives_autres_list(
-    wxr: WiktextractContext,
-    page_data: list[WordEntry],
-    level_node: WikiNode,
+    wxr: WiktextractContext, page_data: list[WordEntry], level_node: LevelNode
 ):
     # drrive to other languages list
     for list_item in level_node.find_child_recursively(NodeKind.LIST_ITEM):
@@ -66,146 +64,168 @@ def process_derives_autres_list(
                 page_data[-1].derived.append(linkage_data)
 
 
-def process_linkage_list(
+def extract_linkage_section(
     wxr: WiktextractContext,
-    page_data: list[WordEntry],
-    level_node: WikiNode,
+    word_entry: WordEntry,
+    level_node: LevelNode,
     linkage_type: str,
     section_tags: list[str] = [],
-) -> None:
+):
     sense_text = ""
     sense_index = 0
-    for template_or_list_node in level_node.find_child_recursively(
-        NodeKind.LIST_ITEM | NodeKind.TEMPLATE
-    ):
-        # list table start template: https://fr.wiktionary.org/wiki/Modèle:(
-        if (
-            isinstance(template_or_list_node, TemplateNode)
-            and template_or_list_node.template_name == "("
-        ):
+    for node in level_node.children:
+        if isinstance(node, TemplateNode) and node.template_name == "(":
             sense_text = clean_node(
-                wxr, None, template_or_list_node.template_parameters.get(1, "")
+                wxr, None, node.template_parameters.get(1, "")
             )
-            sense_index_text = template_or_list_node.template_parameters.get(
-                2, "0"
-            )
+            sense_index_text = node.template_parameters.get(2, "0")
             if (
                 isinstance(sense_index_text, str)
                 and sense_index_text.isdecimal()
             ):
                 sense_index = int(sense_index_text)
-            continue
-        # sense could also be in ";" description list
-        if (
-            template_or_list_node.kind == NodeKind.LIST_ITEM
-            and template_or_list_node.sarg in {";", ":"}
-        ):
-            sense_text = clean_node(wxr, None, template_or_list_node.children)
-            index_pattern = r"\s*\((?:sens\s*)?(\d+)\)$"
-            m = re.search(index_pattern, sense_text)
-            if m is not None:
-                sense_text = re.sub(index_pattern, "", sense_text)
-                sense_index = int(m.group(1))
-            continue
-
-        linkage_data = Linkage(word="", tags=section_tags)
-        if len(sense_text) > 0:
-            linkage_data.sense = sense_text
-        if sense_index != 0:
-            linkage_data.sense_index = sense_index
-        pending_tag = ""
-        inside_bracket = False
-        for index, child_node in enumerate(  # remove nested lists
-            template_or_list_node.invert_find_child(NodeKind.LIST, True)
-        ):
-            if isinstance(
-                child_node, TemplateNode
-            ) and child_node.template_name in [
-                "l",
-                "lien",
-                "zh-lien",
-                "zh-lien-t",
-            ]:
-                process_linkage_template(wxr, child_node, linkage_data)
-            elif (
-                isinstance(child_node, WikiNode)
-                and child_node.kind == NodeKind.LINK
-                and not inside_bracket
-            ):
-                linkage_data.word = clean_node(wxr, None, child_node)
-            elif (
-                isinstance(child_node, WikiNode)
-                and child_node.kind == NodeKind.ITALIC
-            ):
-                current_sense = clean_node(wxr, None, child_node).strip("()")
-                if (
-                    len(list(template_or_list_node.filter_empty_str_child()))
-                    == 1
-                ):
-                    linkage_data.word = current_sense
-                elif current_sense.isdecimal():
-                    linkage_data.sense_index = int(current_sense)
-                else:
-                    linkage_data.sense = current_sense
-            elif (
-                isinstance(child_node, TemplateNode)
-                and child_node.template_name == "réf"
-            ):
-                continue
+        elif isinstance(node, WikiNode) and node.kind == NodeKind.BOLD:
+            sense_text = clean_node(wxr, None, node)
+        elif isinstance(node, WikiNode) and node.kind == NodeKind.LIST:
+            # sense could also be in ";" description list
+            if node.sarg in [";", ":"]:
+                for list_item in node.find_child(NodeKind.LIST_ITEM):
+                    sense_text = clean_node(wxr, None, list_item.children)
+                    index_pattern = r"\s*\((?:sens\s*)?(\d+)\)$"
+                    m = re.search(index_pattern, sense_text)
+                    if m is not None:
+                        sense_text = re.sub(index_pattern, "", sense_text)
+                        sense_index = int(m.group(1))
             else:
-                tag_text = (
-                    child_node
-                    if isinstance(child_node, str)
-                    else clean_node(wxr, page_data[-1], child_node)
-                )
-                if (
-                    tag_text.strip() in {",", "/", "(ou"}
-                    and linkage_data.word != ""
-                ):
-                    # list item has more than one word
-                    add_linkage_data(page_data[-1], linkage_type, linkage_data)
-                    linkage_data = Linkage(word="", tags=section_tags)
-                    continue
-                if tag_text.strip().startswith(
-                    "("
-                ) and not tag_text.strip().endswith(")"):
-                    pending_tag = tag_text
-                    inside_bracket = True
-                    continue
-                elif not tag_text.strip().startswith(
-                    "("
-                ) and tag_text.strip().endswith(")"):
-                    tag_text = pending_tag + tag_text
-                    pending_tag = ""
-                    inside_bracket = False
-                elif len(pending_tag) > 0:
-                    pending_tag += tag_text
-                    continue
-
-                if tag_text.strip().startswith("—"):
-                    linkage_data.translation = clean_node(
+                for list_item in node.find_child(NodeKind.LIST_ITEM):
+                    extract_linkage_list_item(
                         wxr,
-                        None,
-                        list(
-                            template_or_list_node.invert_find_child(
-                                NodeKind.LIST, True
-                            )
-                        )[index:],
-                    ).strip("— ")
-                    break
-                elif tag_text.strip().startswith(":"):
-                    sense_text = tag_text.strip().removeprefix(":").strip()
-                    linkage_data.sense = sense_text
-                else:
-                    tags, _ = capture_text_in_parentheses(tag_text)
-                    for tag in tags:
-                        if tag.isdecimal():
-                            linkage_data.sense_index = int(tag)
-                        else:
-                            linkage_data.raw_tags.append(tag)
+                        word_entry,
+                        list_item,
+                        linkage_type,
+                        section_tags,
+                        sense_text,
+                        sense_index,
+                    )
 
-        if len(linkage_data.word) > 0:
-            add_linkage_data(page_data[-1], linkage_type, linkage_data)
+
+def extract_linkage_list_item(
+    wxr: WiktextractContext,
+    word_entry: WordEntry,
+    list_item: WikiNode,
+    linkage_type: str,
+    section_tags: list[str],
+    sense: str,
+    sense_index: int,
+):
+    linkage_data = Linkage(
+        word="", tags=section_tags, sense=sense, sense_index=sense_index
+    )
+    pending_tag = ""
+    inside_bracket = False
+    for index, child_node in enumerate(list_item.children):
+        if isinstance(
+            child_node, TemplateNode
+        ) and child_node.template_name in [
+            "l",
+            "lien",
+            "zh-lien",
+            "zh-lien-t",
+        ]:
+            process_linkage_template(wxr, child_node, linkage_data)
+        elif (
+            isinstance(child_node, WikiNode)
+            and child_node.kind == NodeKind.LINK
+            and not inside_bracket
+        ):
+            linkage_data.word = clean_node(wxr, None, child_node)
+        elif (
+            isinstance(child_node, WikiNode)
+            and child_node.kind == NodeKind.ITALIC
+        ):
+            current_sense = clean_node(wxr, None, child_node).strip("()")
+            if len(list(list_item.filter_empty_str_child())) == 1:
+                linkage_data.word = current_sense
+            elif current_sense.isdecimal():
+                linkage_data.sense_index = int(current_sense)
+            else:
+                linkage_data.sense = current_sense
+        elif (
+            isinstance(child_node, TemplateNode)
+            and child_node.template_name == "réf"
+        ) or (
+            isinstance(child_node, WikiNode)
+            and child_node.kind == NodeKind.LIST
+        ):
+            continue
+        else:
+            tag_text = (
+                child_node
+                if isinstance(child_node, str)
+                else clean_node(wxr, word_entry, child_node)
+            )
+            if (
+                tag_text.strip() in {",", "/", "(ou"}
+                and linkage_data.word != ""
+            ):
+                # list item has more than one word
+                add_linkage_data(word_entry, linkage_type, linkage_data)
+                linkage_data = Linkage(
+                    word="",
+                    tags=section_tags,
+                    sense=sense,
+                    sense_index=sense_index,
+                )
+                continue
+            if tag_text.strip().startswith(
+                "("
+            ) and not tag_text.strip().endswith(")"):
+                pending_tag = tag_text
+                inside_bracket = True
+                continue
+            elif not tag_text.strip().startswith(
+                "("
+            ) and tag_text.strip().endswith(")"):
+                tag_text = pending_tag + tag_text
+                pending_tag = ""
+                inside_bracket = False
+            elif len(pending_tag) > 0:
+                pending_tag += tag_text
+                continue
+
+            if tag_text.strip().startswith("—"):
+                linkage_data.translation = clean_node(
+                    wxr,
+                    None,
+                    list(list_item.invert_find_child(NodeKind.LIST, True))[
+                        index:
+                    ],
+                ).strip("— \n")
+                break
+            elif tag_text.strip().startswith(":"):
+                sense_text = tag_text.strip().removeprefix(":").strip()
+                linkage_data.sense = sense_text
+            else:
+                tags, _ = capture_text_in_parentheses(tag_text)
+                for tag in tags:
+                    if tag.isdecimal():
+                        linkage_data.sense_index = int(tag)
+                    else:
+                        linkage_data.raw_tags.append(tag)
+
+    if len(linkage_data.word) > 0:
+        add_linkage_data(word_entry, linkage_type, linkage_data)
+    for child_list in list_item.find_child(NodeKind.LIST):
+        for child_list_item in child_list.find_child(NodeKind.LIST_ITEM):
+            extract_linkage_list_item(
+                wxr,
+                word_entry,
+                child_list_item,
+                linkage_type,
+                section_tags,
+                sense,
+                sense_index,
+            )
 
 
 def add_linkage_data(
