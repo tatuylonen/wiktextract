3309e1d70bc4dc9b760b43a2e493a1b458646ff8
daxida <galmaw@hotmail.com>
2025-10-15 17:48:17 +0200

    [el] Add Form source test

--- tests/test_el_inflection.py
+++ tests/test_el_inflection.py
@@ -16,6 +16,7 @@ from wikitextprocessor import Wtp
 
 from wiktextract.config import WiktionaryConfig
 from wiktextract.extractor.el.models import WordEntry
+from wiktextract.extractor.el.page import parse_page
 from wiktextract.extractor.el.table import process_inflection_section
 from wiktextract.wxr_context import WiktextractContext
 
@@ -310,6 +311,141 @@ class TestElInflection(TestCase):
             {"form": "αβγούλια", "tags": ["vocative", "plural"]},
         ]
         self.mktest_form_no_raw_tags(raw, expected)
+
+    def test_two_inflection_sources(self):
+        # https://el.wiktionary.org/wiki/αμάξι
+        # Contains forms from both declension and header
+        self.wxr.wtp.add_page("Πρότυπο:-el-", 10, "Νέα ελληνικά (el)")
+        self.wxr.wtp.add_page("Πρότυπο:ουσιαστικό", 10, "Ουσιαστικό")
+        self.wxr.wtp.add_page(
+            "Πρότυπο:el-κλίση-'τραγούδι'",
+            10,
+            """{| style="clear%3Aright%3B+float%3Aright%3B+margin-left%3A0.5em%3B+margin-bottom%3A0.5em%3Bbackground%3A%23ffffff%3B+color%3A%23000000%3B+border%3A1px+solid%23a1bdea%3B+text-align%3Aright%3B" rules="none" border="1" cellpadding="3" cellspacing="0"
+|-
+! style="background%3A%23a1bdea%3B+border-right%3A1px+solid+%23c1d3f1%3B+text-align%3Acenter%3B+font-size%3A90%25%3B" align="center" | &darr;&nbsp;''πτώσεις''
+! style="background%3A%23a1bdea%3B+border-right%3A1px+solid+%23c1d3f1%3B" colspan="2" align="center" | &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;[[ενικός|<span title="%CE%B5%CE%BD%CE%B9%CE%BA%CF%8C%CF%82+%CE%B1%CF%81%CE%B9%CE%B8%CE%BC%CF%8C%CF%82" style="color%3Ablack%3B">'''''ενικός'''''</span>]]&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+! style="background%3A%23a1bdea%3B" colspan="2" align="center" | &nbsp;&nbsp;[[πληθυντικός|<span title="%CF%80%CE%BB%CE%B7%CE%B8%CF%85%CE%BD%CF%84%CE%B9%CE%BA%CF%8C%CF%82+%CE%B1%CF%81%CE%B9%CE%B8%CE%BC%CF%8C%CF%82" style="color%3Ablack%3B">'''''πληθυντικός'''''</span>]]&nbsp;&nbsp;
+|-
+| style="background%3A%23d5e2f6%3B+border-bottom%3A1px+solid+%23eaf0fa%3B+text-align%3Aright%3B+font-size%3A90%25%3B" | [[ονομαστική|<span title="%CE%BF%CE%BD%CE%BF%CE%BC%CE%B1%CF%83%CF%84%CE%B9%CE%BA%CE%AE+%CF%80%CF%84%CF%8E%CF%83%CE%B7" style="color%3Ablack%3B">'''''ονομαστική'''''</span>]]
+| style="border-bottom%3A1px+solid+%23eaf0fa%3B" align="center" | [[το|<span style="color%3A%23002000%3B+font-weight%3Anormal%3B">το</span>]]
+| style="border-bottom%3A1px+solid+%23eaf0fa%3B+border-right%3A1px+solid+%23eaf0fa%3B" align="left" | [[αμάξι|<span style="color%3A%23002000%3B+font-weight%3Anormal%3B">αμάξ</span><span style="color%3A%23EB0000%3B+font-weight%3Anormal%3B">ι</span>]]
+| style="border-bottom%3A1px+solid+%23eaf0fa%3B" align="center" | [[τα|<span style="color%3A%23002000%3B+font-weight%3Anormal%3B">τα</span>]]
+| style="border-bottom%3A1px+solid+%23eaf0fa%3B" align="left" | [[αμάξια|<span style="color%3A%23002000%3B+font-weight%3Anormal%3B">αμάξ</span><span style="color%3A%23EB0000%3B+font-weight%3Anormal%3B">ια</span>]]
+|-
+| style="background%3A%23d5e2f6%3B+border-bottom%3A1px+solid+%23eaf0fa%3B+text-align%3Aright%3B+font-size%3A90%25%3B" | &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;[[γενική|<span title="%CE%B3%CE%B5%CE%BD%CE%B9%CE%BA%CE%AE+%CF%80%CF%84%CF%8E%CF%83%CE%B7" style="color%3Ablack%3B">'''''γενική'''''</span>]]
+| style="border-bottom%3A1px+solid+%23eaf0fa%3B" align="center" | [[του|<span style="color%3A%23002000%3B+font-weight%3Anormal%3B">του</span>]]
+| style="border-bottom%3A1px+solid+%23eaf0fa%3B+border-right%3A1px+solid+%23eaf0fa%3B" align="left" | [[αμαξιού|<span style="color%3A%23002000%3B+font-weight%3Anormal%3B">αμαξ</span><span style="color%3A%23EB0000%3B+font-weight%3Anormal%3B">ιού</span>]]
+| style="border-bottom%3A1px+solid+%23eaf0fa%3B" align="center" | [[των|<span style="color%3A%23002000%3B+font-weight%3Anormal%3B">των</span>]]
+| style="border-bottom%3A1px+solid+%23eaf0fa%3B" align="left" | [[αμαξιών|<span style="color%3A%23002000%3B+font-weight%3Anormal%3B">αμαξ</span><span style="color%3A%23EB0000%3B+font-weight%3Anormal%3B">ιών</span>]]
+|-
+| style="background%3A%23d5e2f6%3B+border-bottom%3A1px+solid+%23eaf0fa%3B+text-align%3Aright%3B+font-size%3A90%25%3B" | &nbsp;&nbsp;&nbsp;&nbsp;[[αιτιατική|<span title="%CE%B1%CE%B9%CF%84%CE%B9%CE%B1%CF%84%CE%B9%CE%BA%CE%AE+%CF%80%CF%84%CF%8E%CF%83%CE%B7" style="color%3Ablack%3B">'''''αιτιατική'''''</span>]]
+| style="border-bottom%3A1px+solid+%23eaf0fa%3B" align="center" | [[το|<span style="color%3A%23002000%3B+font-weight%3Anormal%3B">το</span>]]
+| style="border-bottom%3A1px+solid+%23eaf0fa%3B+border-right%3A1px+solid+%23eaf0fa%3B" align="left" | [[αμάξι|<span style="color%3A%23002000%3B+font-weight%3Anormal%3B">αμάξ</span><span style="color%3A%23EB0000%3B+font-weight%3Anormal%3B">ι</span>]]
+| style="border-bottom%3A1px+solid+%23eaf0fa%3B" align="center" | [[τα|<span style="color%3A%23002000%3B+font-weight%3Anormal%3B">τα</span>]]
+| style="border-bottom%3A1px+solid+%23eaf0fa%3B" align="left" | [[αμάξια|<span style="color%3A%23002000%3B+font-weight%3Anormal%3B">αμάξ</span><span style="color%3A%23EB0000%3B+font-weight%3Anormal%3B">ια</span>]]
+|-
+| style="background%3A%23d5e2f6%3B+text-align%3Aright%3B" | &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;[[κλητική|<span title="%CE%BA%CE%BB%CE%B7%CF%84%CE%B9%CE%BA%CE%AE+%CF%80%CF%84%CF%8E%CF%83%CE%B7" style="color%3Ablack%3B+font-size%3A90%25%3B">'''''κλητική'''''</span>]]
+| align="center" |
+| style="border-right%3A1px+solid+%23eaf0fa%3B" align="left" | [[αμάξι|<span style="color%3A%23002000%3B+font-weight%3Anormal%3B">αμάξ</span><span style="color%3A%23EB0000%3B+font-weight%3Anormal%3B">ι</span>]]
+| align="center" |
+| align="left" | [[αμάξια|<span style="color%3A%23002000%3B+font-weight%3Anormal%3B">αμάξ</span><span style="color%3A%23EB0000%3B+font-weight%3Anormal%3B">ια</span>]]
+|-
+| colspan="5" align="left" style="background%3A%23eaf0fa%3B+font-size%3A80%25%3B+line-height%3A100%25%3B" | Οι καταλήξεις -ιού, -ια, -ιών προφέρονται με [[συνίζηση]].
+|-
+| colspan="5" align="right" style="text-align%3Aright%3B+background%3A%23eaf0fa%3B+font-size%3A70%25%3B+line-height%3A100%25%3B" | [[:Κατηγορία:Ουσιαστικά που κλίνονται όπως το 'τραγούδι' (νέα ελληνικά)|Κατηγορία]]  όπως «[[Παράρτημα:Ουσιαστικά (νέα ελληνικά)/ουδέτερα#τραγούδι|τραγούδι]]» - [[Παράρτημα:Ουσιαστικά (νέα ελληνικά)|<span title="%CE%A0%CE%B1%CF%81%CE%AC%CF%81%CF%84%CE%B7%CE%BC%CE%B1%3A%CE%9F%CF%85%CF%83%CE%B9%CE%B1%CF%83%CF%84%CE%B9%CE%BA%CE%AC">Παράρτημα:Ουσιαστικά</span>]]
+|}""",
+        )
+        self.wxr.wtp.add_page(
+            "Πρότυπο:ο",
+            10,
+            """<span style="background:#ffffff; color:#002000;">''ουδέτερο''</span>""",
+        )
+
+        raw = """
+=={{-el-}}==
+{{el-κλίση-'τραγούδι'}}
+==={{ουσιαστικό|el}}===
+'''{{PAGENAME}}''' {{ο}}
+* foo
+        """.strip()
+
+        word = "αμάξι"
+        page_datas = parse_page(self.wxr, word, raw)
+        received = page_datas[0]["forms"]
+
+        expected = [
+            {
+                "form": "el-κλίση-'τραγούδι'",
+                "tags": ["inflection-template"],
+                "source": "declension",
+            },
+            {
+                "form": "αμάξι",
+                "tags": ["nominative", "singular"],
+                "raw_tags": ["ενικός", "ονομαστική"],
+                "source": "declension",
+            },
+            {
+                "form": "αμάξια",
+                "tags": ["nominative", "plural"],
+                "raw_tags": ["ονομαστική", "πληθυντικός"],
+                "source": "declension",
+            },
+            {
+                "form": "αμαξιού",
+                "tags": ["genitive", "singular"],
+                "raw_tags": ["ενικός", "γενική"],
+                "source": "declension",
+            },
+            {
+                "form": "αμαξιών",
+                "tags": ["genitive", "plural"],
+                "raw_tags": ["γενική", "πληθυντικός"],
+                "source": "declension",
+            },
+            {
+                "form": "αμάξι",
+                "tags": ["accusative", "singular"],
+                "raw_tags": ["ενικός", "αιτιατική"],
+                "source": "declension",
+            },
+            {
+                "form": "αμάξια",
+                "tags": ["accusative", "plural"],
+                "raw_tags": ["αιτιατική", "πληθυντικός"],
+                "source": "declension",
+            },
+            {
+                "form": "αμάξι",
+                "tags": ["singular", "vocative"],
+                "raw_tags": ["ενικός", "κλητική"],
+                "source": "declension",
+            },
+            {
+                "form": "αμάξια",
+                "tags": ["plural", "vocative"],
+                "raw_tags": ["κλητική", "πληθυντικός"],
+                "source": "declension",
+            },
+            {
+                "form": "αμάξι",
+                "tags": ["neuter"],
+                "raw_tags": ["ουδέτερο"],
+                "source": "header",
+            },
+        ]
+
+        def normalize_forms(lst):
+            for form in lst:
+                if "raw_tags" in form:
+                    form["raw_tags"].sort()
+                if "tags" in form:
+                    form["tags"].sort()
+
+        normalize_forms(received)
+        normalize_forms(expected)
+
+        self.assertEqual(received, expected)
 
     def test_noun_inflection_standard(self):
         raw = """
