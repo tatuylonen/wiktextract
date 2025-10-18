96c927ea8661cc8f9a6c53b690233dbcbe99f130
daxida <galmaw@hotmail.com>
2025-10-18 13:13:49 +0200

    [el] Add warning to test_el_tags

--- tests/test_el_tags.py
+++ tests/test_el_tags.py
@@ -6,6 +6,20 @@ from wiktextract.topics import valid_topics
 
 
 class TestElTags(TestCase):
+    """WARNING:
+
+    The results differ when running these tests via unittest or manually here
+    as TestElTags().test_validate_tags. This is due to side effects adding more
+    tags/topics when running it via unittest.
+
+    As of the writting of this warning, there are (standalone/unittest):
+    - Tags   1315 / 3365
+    - Topics 227  / 854
+
+    This explains why some topics like "computer" pass these tests, even though
+    only "computing" is found in the original valid_topics.
+    """
+
     def test_validate_tags(self) -> None:
         # Check validity of manually entered tags in tag_map.
         # valid_tags is from higher level code, originally created for the English
@@ -21,10 +35,12 @@ class TestElTags(TestCase):
                         self.assertFalse(f"Invalid tag in tag_map: {tag=}")
 
     def test_validate_topics(self) -> None:
-        for tags in topic_map.values():
-            for tag in tags:
-                for part in tag.split("-"):
+        for topics in topic_map.values():
+            for topic in topics:
+                for part in topic.split("-"):
                     if not part.isalpha() or (
-                        part.islower() and tag not in valid_topics
+                        part.islower() and topic not in valid_topics
                     ):
-                        self.assertFalse(f"Invalid topic in topic_map: {tag=}")
+                        self.assertFalse(
+                            f"Invalid topic in topic_map: {topic=}"
+                        )
